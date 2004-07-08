ff52ead07980a3ac158fae722760ebace599d200
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2004-07-08 10:56:11 +0000

    basis=89/99

--- arrival.pm
+++ arrival.pm
@@ -9,9 +9,10 @@ my $speed=1+0.16*$$options{racespeed};
 #my ($c,$a)=(14.04,33.97);
 #my ($c,$a)=(14.135,33.93);
 my ($c,$a)=(14.248,33.88);
+my $e=(89/99); # instead of 90/100
 my $basetime=$c+sqrt(sqrt($$options{distance})+$$options{planet}*0.1)*$a;
 
-my $time=$basetime/$speed*0.9**$$options{energy};
+my $time=$basetime/$speed*$e**$$options{energy};
 if($$options{own}) {$time/=2}
 
 return $time;
