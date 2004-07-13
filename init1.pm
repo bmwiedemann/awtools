a42de4d01d149efef9c08efd2cb8366c3ec1204a
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2004-07-13 12:31:11 +0000

    initial checkin sets default planetary and player values

---
+++ init1.pm
@@ -0,0 +1,14 @@
+$planet[0]{pp}=3;
+$planet[0]{pop}=1;
+$planet[0]{hf}=0;
+$planet[0]{rf}=0;
+$planet[0]{rl}=0;
+$planet[0]{gc}=0;
+$planet[0]{sb}=0;
+
+$player{racepop}=1+0.13*0;
+$player{racepp}=1+0.05*0;
+$player{racecul}=1+0.05*0;
+$player{racesci}=1+0.11*0;
+
+1;
