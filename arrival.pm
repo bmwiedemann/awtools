c93d4a7234e1c2922fbe84d4a265e00351d27b3a
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2004-07-26 16:47:14 +0000

    beta7 update

--- arrival.pm
+++ arrival.pm
@@ -4,11 +4,11 @@ use strict;
 
 # calculates and returns travel time for given options
 sub traveltime { my ($options)=@_;
-my $speed=1+0.16*$$options{racespeed};
+my $speed=1+0.19*$$options{racespeed};
 #my ($c,$a)=(14.0,34.0);
 #my ($c,$a)=(14.04,33.97);
 #my ($c,$a)=(14.135,33.93);
-my ($c,$a)=(14.248,33.88);
+my ($c,$a)=(14.248,34.13);
 my $e=(89/99); # instead of 90/100
 my $basetime=$c+sqrt(sqrt($$options{distance})+$$options{planet}*0.1)*$a;
 
