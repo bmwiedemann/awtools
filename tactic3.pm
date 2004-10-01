4126d8e79dfe035fce6c01b1a5f876603331b38a
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2004-10-01 12:17:47 +0000

    improvements

--- tactic3.pm
+++ tactic3.pm
@@ -1,18 +1,31 @@
-my %rentability=qw"hf 1.2 rf 1.5 gc 1 rl 0.8 sb 0.4";
+my %rentability=qw"hf 0.9 rf 1.7 gc 1 rl 0.7 sb 0.4";
+my %max=qw(hf 13 rf 13 gc 99 rl 13 sb 0);
+#my %rentability=qw"hf 1.2 rf 1.5 gc 1 rl 0.8 sb 0.0004";
 #my %rentability=qw"hf 0.9 rf 1 gc 1 rl 1 sb 1";
 
 # returns value>0
 sub rentability($$){my ($p,$building)=@_;
+  my $rf=$$p{"rf"};
+  my $result=$rentability{$building};
+  if($$p{$building}>=$max{$building}) {return 0.00001}
   if($building eq "gc") {
-    return $rentability{$building}+$$p{"rf"}/15;
+    $result+=$rf/15;
   }
-  return $rentability{$building};
+  if($building eq "rl") {
+    $result+=$rf/35;
+  }
+  if($building eq "rl" and $rf<6) {return 0.0001}
+  if($building eq "rf" or $building eq "rl") {
+    if(buildcost($$p{$building})>1300-$turn/$updatetime) { return 0.0001; }
+  }
+  return $result;
 }
 
 sub findtarget($) { my($p)=@_;
   my ($min,$mint);
   for my $t(@buildings) {
-    my $val=$prod[$$p{$t}+1]/rentability($p,$t);
+    if($$p{$t}>=$max{$t}){next}
+    my $val=buildcost($$p{$t})/rentability($p,$t);
 #    print "$t:$val ";
     if(!defined($min) || $val<$min) {
       $min=$val;
@@ -26,10 +39,21 @@ sub findtarget($) { my($p)=@_;
 sub spend1()
 { 
   foreach my $p (@planet) {
-    my $val=$$p{rf}+$$p{pop};
+#    my $val=$$p{rf}+$$p{pop};
+    if(int($player{cul})>@planet) {
+      my $n=@planet;
+      $planet[0]{pp}-=60;
+      initplanet(\%{$planet[$n]});
+      if($planet[0]{pp}>0) {
+        my $rate=1.0-$n*0.05;
+	if($rate<0.2) {$rate=0.2}
+	$planet[$n]{pp}=$planet[0]{pp}*$rate;
+        $planet[0]{pp}=0;
+      }
+    }
     do {
       $target=findtarget($p);
-    } while(build($p,$target,0));
+    } while(defined($target) && build($p,$target,0));
   }
 }
 
