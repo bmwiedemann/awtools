db1156b090f6456f3a24873669c72a79aeab8186
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2004-10-03 02:55:37 +0000

    initial checkin create AW map from starmap.csv

---
+++ drawmap.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl -w
+#
+# draw AW map
+#
+use Image::Magick;
+use strict;
+
+my $imagesize=190;
+my $ih=$imagesize/2;
+my $file=shift(@ARGV) || die "need input\n";
+my $fileend=$file;
+$fileend=~s".*/"";
+$fileend=~s/(\d\d)-(\d\d)-(\d+)\.tar\.bz2/$3-$2-$1/;
+my $out="out/$fileend";
+my $axiscolor="blue";
+my $c=25; # base color
+
+sub maptoimg($$) { my($x,$y)=@_;
+ return ($x+$ih,$y+$ih);}
+sub imgtomap($$) { my($x,$y)=@_;
+ return ($x-$ih,$y-$ih);}
+
+
+system(qw"tar xjf",$file);
+print "reading csv...\n";
+require "input.pm";
+
+#open(IN, "< $file") or die $!;
+#my @coor=();
+#while(<IN>) {
+#	s/#.*//;
+#	if(/^\s*$/){next}
+#	my @a=split(" ");
+#	push(@coor, [@a[0,1]]);
+#}
+
+# Create the main image
+my $im = new Image::Magick;
+$im->Set(size=>$imagesize . 'x' . $imagesize);
+$im->Read('xc:black');
+print "Drawing...\n";
+my $img=$im->Clone();
+
+sub pixel($$$) {my($x,$y,$c)=@_;
+  $img->Set("pixel[$x,$y]", $c);
+}
+
+
+
+for(my $px=0; $px<$imagesize; $px++) {
+ for(my $py=0; $py<$imagesize; $py++) {
+	my ($x,$y)=imgtomap($px,$py);
+	my $v=0; # color value
+	if(defined($::starmap{"$x,$y"})) {
+		$v=$::starmap{$::starmap{"$x,$y"}}{"level"}/3;
+	}
+	if($v>0){$v=$c+$v*int((255-$c)/8)}
+	if($v>255){$v=255}
+#	print $v," ";
+	$v=sprintf("#%.2x%.2x%.2x", $v,$v,$v);
+	pixel($px,$py,$v);
+}}
+#foreach(@coor){
+#	my ($kx,$ky)=@$_;
+#	my ($px,$py)=maptoimg($kx,$ky);
+#	pixel($px,$py,"green");
+#}
+
+
+$img->Draw(fill=>'none',stroke=>$axiscolor,primitive=>'line', points=>"0,$ih $imagesize,$ih",strokewidth=>1);
+$img->Draw(fill=>'none',stroke=>$axiscolor,primitive=>'line', points=>"$ih,0 $ih,$imagesize",strokewidth=>1);
+
+$img->Write("$out.png");
+#$img->Write('win:');
+
---
+++ input.pm
@@ -0,0 +1,59 @@
+#!/usr/bin/perl -w
+
+use strict "vars";
+our (%alliances,%starmap,%player,%playerid);
+my (@elements);
+sub starmap { my($x,$y,$level,$id,$name)=@_;
+	$starmap{$id}={"x"=>$x, "y"=>$y, "level"=>$level, "name"=>$name};
+	$starmap{$name}=$id;
+	$starmap{"$x,$y"}=$id;
+}
+sub alliances {
+	my %h=();
+	my $id;
+	splice(@elements,5,1);
+	for(my $i=0; $i<=$#elements; ++$i) {
+		if($elements[$i] eq "id") {$id=$_[$i]}
+		else {$h{$elements[$i]}=$_[$i];}
+		if($elements[$i] eq "tag") {$alliances{$_[$i]}=$id}
+	}
+	$alliances{$id}=\%h;
+}
+sub player { #rank points id science culture level home_id logins from joined alliance name
+	my %h=();
+	my $id;
+	splice(@_,7,1);
+	for(my $i=0; $i<=$#elements; ++$i) {
+		if($elements[$i] eq "id") {$id=$_[$i]}
+		else {$h{$elements[$i]}=$_[$i];}
+		if($elements[$i] eq "name") {$playerid{$_[$i]}=$id}
+	}
+	$player{$id}=\%h;
+}
+
+for my $f (qw(starmap)) {
+#for my $f (qw(alliances starmap player)) {
+	my $file="$f.csv";
+	my $head=1;
+	open(F, $file) or die "could not open $file: $!";
+	while(<F>) {
+		chomp();
+		my @a=split ("\t", $_);
+		if($head) {
+			@elements=@a;
+			$head=0;
+			next;
+		}
+		#print "$f $_\n";
+		&$f(@a);
+	}
+}
+#my $x=$starmap{$starmap{"0,0"}};
+#my $x=$alliances{$alliances{"TGD"}};
+#my $x=$player{49545};
+#foreach(keys(%$x)) {
+#	print "$_ = $$x{$_}\n";
+#}
+#print $playerid{"greenbird"},"\n";
+
+1;
