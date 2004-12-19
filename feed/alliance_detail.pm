726a2a7d9169fce0af5b21cd2a1a50213beb90ee
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2004-12-19 16:04:46 +0000

    initial checkin parser modules for feedupdate

---
+++ feed/alliance_detail.pm
@@ -0,0 +1,103 @@
+my $debug=$::options{debug};
+use Time::Local;
+print "alliance_detail\n<br>";
+if($debug) {print "debug mode - no modifications done<br>\n"}
+
+my $dbname="/home/bernhard/db/$ENV{REMOTE_USER}-planets.dbm";
+my $dbname2="/home/bernhard/db/$ENV{REMOTE_USER}-relation.dbm";
+use DB_File;
+require "./input.pm";
+if(!/<tr><td><center>([^<]*)<br>/) {return 1;}
+my $name=$1;
+my $pid=playername2id($name);
+if(!$pid) {print "user $name not found<br>\n";return 1}
+print qq!user <a href="relation?name=$name">$name($pid)</a><br>\n!;
+my $name2="\L$name";
+
+my %data;
+tie(%data, "DB_File", $dbname) or print "error accessing DB\n";
+my %data2;
+tie(%relation, "DB_File", $dbname2) or print "error accessing DB\n";
+
+#my $science="";
+my @science;
+foreach my $sci (@::sciencestr) {
+	next if ! m,$sci</td><td>(\d+),; #$sci{$sci}=$1;
+	my $val=$1;
+	push(@science,$val);
+	#$sci=~/^(...)/;$sci=$1;
+	#$science.=" $sci=$val";
+}
+my @race;
+{
+	my $racere="";
+	foreach my $r (@::racestr) {
+		$racere.=qr"<li>[+-]\d+% $r \(([+-]\d)\)</li>";
+	}
+	if(/$racere/) {@race=($1,$2,$3,$4,$5,$6,$7);}
+}
+my $oldentry=$relation{$name2};
+my $newentry=addplayerir($oldentry,\@science,\@race);
+if($debug){ print "$oldentry @race @science new:$newentry\n<br>" }
+else {$relation{$name2}=$newentry}
+untie %relation;
+
+
+my @a;
+for(;(@a=m!<tr[^>]*><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td>(?:<td>N/A</td>){5}<td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td></tr>(.*)!); $_=$a[8]) {
+	my $sid="$a[0]#$a[1]";
+	my $details="@a[3..7]";
+	my $oldentry=$data{$sid};
+	my $time=gmtime();
+	if($oldentry=~/$details/){$oldentry=~s/^3 $pid /4 $pid /;next}
+	$details="automagic:$name:$time $details";
+	my $newentry=$oldentry||"4 $pid";
+	$newentry.=" $details";
+	print "sieged: ".planetlink($sid)."<br>\n";
+	if(!$debug){$data{$sid}=$newentry;}
+	else {print "$sid $newentry<br>\n"}
+}
+for(;(@a=m!<tr[^>]*><td>(\d+)</td><td>(\d+)</td><td colspan=[^>]*>([^<]*)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)(.*)!); $_=$a[8]) {
+	my $sid="$a[0]#$a[1]";
+	my $details="@a[3..7]";
+	print "targeted: ".planetlink($sid)." $details <br>\n";
+	my $oldentry=$data{$sid};
+	my $time=parseawdate($a[2]);
+	$time=gmtime($time-3600*$::options{tz});
+	$details="automagic:$name:$time $details";
+	#print "old: ",$oldentry;
+	my $newentry=$oldentry||"3 $pid";
+	if($oldentry=~/$details/) {next}
+	$newentry.=" $details";
+	#print "new: $newentry<br>";
+	if(!$debug){$data{$sid}=$newentry;}
+}
+for(;(@a=m!([^>]*)</td><td[^>]*> <b>Attention(.*?) going to attack <b>([^<]+)</b>.<br>We suppose its the Fleet of <a href=/0/Player/Profile.php/\?id=(\d+)>([^<]+)</a>((?:[^>]*</td><td[^>]*> <b>Attention)?.*)!);$_=$a[5]) {
+	my @ship=(0,0,0,0,0);
+	my $pid=$a[3];
+	my $sid;
+	my $shipn=0;
+	foreach my $ship (qw(Transports Colony Destroyer Cruiser Battleship)) {
+		if($a[1]=~/(\d+) $ship/) {
+			$ship[$shipn]=$1;
+		}
+		$shipn++;
+	}
+	my $time=parseawdate($a[0]);
+	$time=gmtime($time-3600*$::options{tz});
+	if($a[2]=~/(.+) (\d+)$/) {
+		$sid=systemname2id($1);
+		if($sid){$sid.="#$2"}
+	}
+	if(!$sid) {next}
+	my $oldentry=$data{$sid};
+	my $newentry;
+	my $details="automagic:$a[4]:$time @ship";
+	if($oldentry=~/$details/) {next}
+	if($oldentry) {$newentry=$oldentry." ".$details}
+	else {$newentry="3 $pid ".$details}
+	print "incoming: ".planetlink($sid)."<br>\n";
+	#print " old:$oldentry new:$newentry<br>\n";
+	if(!$debug){$data{$sid}=$newentry;}
+}
+1;
---
+++ feed/player_profile.pm
@@ -0,0 +1,79 @@
+use strict;
+my $dbname="/home/bernhard/db/$ENV{REMOTE_USER}-relation.dbm";
+use DB_File;
+#require "standard.pm";
+my %relation;
+my %timevalue=(second=>1, minute=>60, hour=>3600, day=>86400);
+my $debug=$::options{debug};
+if($debug) {print "debug mode - no modifications done<br>\n"}
+
+print "player feed<br>\n";
+m,>\s*([^ <]+)(?: \(\d+[^)<]*\)</font>)?(?:<br><small>Premium Member</small>)?</b></center>,; my $name=$1;
+if($name && m,Idle[^0-9\n]*(\d+|(?:N/A))(\s+seconds?|\s+minutes?|\s+hours?|\s+days?|),){
+	my $idle="$1 $2";
+	my $idlei=$1;
+	my $timestr=$2; $timestr=~s/s$//; $timestr=~s/^\s*//;
+	$idlei*=$timevalue{$timestr};
+	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday)=gmtime();
+	$hour+=$::options{tz};
+	my $servertime=$hour*3600+$min*60+$sec;
+        #print scalar gmtime(), " GMT ";#: $name idle:$1 $2  ";
+	m,Local Time</td><td>(\d\d):(\d\d)</td></tr>,; my $localtime=($1*60+$2)*60+30;
+	my $deliverytime=($servertime-$localtime);
+	if($deliverytime<-24*60*60+50*60) {$deliverytime+=24*60*60}
+	if($deliverytime<-60 || $deliverytime>50*60) {
+		print "version outdated or wrong timezone?";
+		exit(0);
+	}
+	$idlei+=$deliverytime;
+	my $lastonline=time()-$idlei;
+	my $inaccuracy=$timevalue{$timestr}+30;
+	
+        m,Logins</td><td>(\d+),; my $logins=$1;
+	m,Playerlevel</a></td><td>(\d+ - \d+%)</td></tr>,; my $pl=$1;
+#        my $points=0;
+#        if(m,Rank \(Points Scored\)</td><td>([^<]*),){$points=$1};
+#        m,Playerlevel</td><td>(\d+),; my $pl=$1;
+        m,Sciencelevel</td><td>(\d+),; my $sl=$1;
+        m,Culturelevel</td><td>(\d+),; my $cl=$1;
+	#my $science="";
+	my @science;
+	foreach my $sci (@::sciencestr) {
+		next if ! m,$sci</td><td>(\d+),; #$sci{$sci}=$1;
+		#my $val=$1;
+		push(@science,$1);
+		#$sci=~/^(...)/;$sci=$1;
+		#$science.=" $sci=$val";
+	}
+	my @race;
+	{
+		my $racere="";
+		foreach my $r (@::racestr) {
+			$racere.=qr"<li>[+-]\d+% $r \(([+-]\d)\)</li>";
+		}
+		if(/$racere/) {@race=($1,$2,$3,$4,$5,$6,$7);print "race: @race<br>\n"}
+	}
+	my $lastonlinegmt=gmtime($lastonline);
+	print qq! <a href="relation?name=$name">name=$name</a> idle=$idle time=$localtime last=&quot;$lastonlinegmt&quot; logins=$logins pl=$pl sl=$sl cl=$cl\n<br>!;
+	tie(%relation, "DB_File", $dbname) or print "error accessing DB\n";
+	$name="\L$name";
+	my $oldentry=$relation{$name};
+	#if(!$oldentry) {$oldentry="4 UNKNOWN "}
+	#$oldentry=~/(\d+ \w+ .*)(?=$::magicstring)(.*)/s; 
+	#my ($rest,$magic)=($1,$2);
+	#if($oldentry!~/$::magicstring/) {$magic=""; $rest=$oldentry;}
+	#print "$dbname:$name $oldentry $rest + magic:$magic";
+	#my $prevlogins="";
+	#my $race="";
+	#if($magic=~/(login:.*)/) {$prevlogins=$1." ";}
+	#if($magic=~/(race:[-+,0-9]+)/) {$race=$1." ";}
+	#if(!$race) {$race="race:".join(",",@race)." "}
+	#my $newentry="${prevlogins}login:$logins:$lastonline+$inaccuracy";
+#"${magicstring}";
+	#chomp($rest);
+	#$newentry="$rest\n${::magicstring}pl=$pl sl=$sl cl=$cl$science $race$newentry";
+	my $newentry=addplayerir($oldentry, \@science, \@race, "$logins:$lastonline+$inaccuracy");
+	if(!$debug) {$relation{$name}=$newentry;}
+	else {print "<br>new:",$newentry;}
+}
+
