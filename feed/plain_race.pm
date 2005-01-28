2b753f61ddf43a472ce74ecc19c1f3d26947ddbb
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2005-01-28 05:43:43 +0000

    added science + race parser (for copy+paste without HTML)

---
+++ feed/plain_race.pm
@@ -0,0 +1,32 @@
+use strict;
+use DB_File;
+my $dbname="/home/bernhard/db/$ENV{REMOTE_USER}-relation.dbm";
+
+my $racere="";
+my $sciencere="";
+my @science;
+my @race;
+foreach my $r (@::racestr) {
+	$racere.=qr"\*?\s*[+-]?\d+%\s+$r\s+\(([+-]?\d)\)\s*"s;
+}
+foreach my $sci (@::sciencestr) {
+	$sciencere.=qr"$sci\s+(\d+)\s*"s;
+}
+#print "$_ $racere";
+my $name="\L$::options{name}";
+if(@race=/$racere/) {
+	print qq! <a href="relations?name=$::options{name}">name=$::options{name}</a> race: @race<br>\n!;
+}
+if(@science=/$sciencere/) {
+	print "science: @science<br>\n";
+}
+if(@race || @science) {
+	my %relation;
+	tie(%relation, "DB_File", $dbname) or print "error accessing DB\n";
+	my $oldentry=$relation{$name};
+	my $newentry=addplayerir($oldentry, \@science, \@race);
+	if($::options{debug}) {print $newentry}
+	else {$relation{$name}=$newentry}
+	exit 0;
+}
+1;
