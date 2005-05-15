70399906cbf4422d620f27d01a5c3e06851e94cc
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2005-05-15 17:51:25 +0000

    change sorting

--- holes.pl
+++ holes.pl
@@ -4,7 +4,7 @@ use strict;
 use CGI ":standard";
 require "input.pm";
 
-sub holesort { $$a[3]<=>$$b[3] }
+sub holesort { $$a[3]<=>$$b[3] || $$b[1]<=>$$a[2]}
 
 my @holes;
 {my $x=$::planets}
@@ -12,24 +12,28 @@ my $head=AWheader2("holes list");
 $head=~s!index!/cgi-bin/index!;
 print $head.
 "sys: members:friends:others<br>\n";
-for my $sid (1..4000) {
+for my $sid (1..4600) {
 	my $friend=0;
 	my $other=0;
 	my $member=0;
+	my $worstrel=10;
 	foreach my $planet (@{$::planets{$sid}}) {
 		my $p=$$planet{ownerid};
 		my @rel=getrelation(playerid2name($p));
-		if($rel[0] && $rel[0]>=5) {
+		if(!$p || $p<=2 || !$rel[0]) {$rel[0]=4}
+		if($rel[0]<$worstrel) {$worstrel=$rel[0]}
+		if($rel[0]>=5) {
 			$friend++;
 			if($rel[0]==9) {$member++}
 		} else {$other++}
 	}
-	next if $member<3 || $friend==12;
-	push(@holes, [$sid, $member, $friend, $other]);
+	next if $member<3 || $member==12;
+	push(@holes, [$sid, $member, $friend, $other, $worstrel]);
 }
 
 foreach(sort holesort @holes) {
-	my ($sid, $member, $friend, $other)=@$_;
-	print qq!<a href="/cgi-bin/system-info?id=$sid">$sid: $member:$friend:$other</a><br>\n!;
+	my ($sid, $member, $friend, $other, $worstrel)=@$_;
+	my $c=getrelationcolor($worstrel);
+	print span({-style=>"color: $c"},"status ").qq!<a href="/cgi-bin/system-info?id=$sid">$sid: $member:$friend:$other</a>!.br."\n";
 }
 print "</body></html>\n";
