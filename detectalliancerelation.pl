19ebd4aa897a3c539375d3fae6a476bd8207f4c4
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2005-06-10 09:25:58 +0000

    use minpop instead of system-level

--- detectalliancerelation.pl
+++ detectalliancerelation.pl
@@ -8,26 +8,29 @@ my %nsystems;
 for my $sid (1..6000) {
  my %allis=();
  my $level=systemid2level($sid);
+ my $minpop=100;
  for my $plid (1..12) {
 	my $p=getplanet($sid, $plid);
-	my $o=$$p{ownerid};
+	my $o=planet2owner($p);
+	my $pop=planet2opop($p);
 	if(!$o || $o<=2) {next}
+	if($pop<$minpop) {$minpop=$pop}
 	my $aid=playerid2alliance($o);
 	if(!$aid) {next}
-	if($::alliances{$aid}{points}<270 && allianceid2members($aid)<10) {next}
+	if($::alliances{$aid}{points}<270 && allianceid2members($aid)<8) {next}
 	$allis{$aid}++;
  }
  foreach my $a1 (keys %allis) {
   foreach my $a2 (keys %allis) {
 	next if $a1==$a2;
-	$relation{"$a1,$a2"}+=$level;#*$allis{$a1}*$allis{$a2};
+	$relation{"$a1,$a2"}+=$minpop;#*$allis{$a1}*$allis{$a2};
 	$nsystems{"$a1,$a2"}++;
   }
  }
 }
 
-sub sortfunc { return $relation{$b}<=>$relation{$a} }
-#sub sortfunc { return $nsystems{$b}<=>$nsystems{$a} }
+#sub sortfunc { return $relation{$b}<=>$relation{$a} }
+sub sortfunc { return $nsystems{$b}<=>$nsystems{$a} }
 
 foreach my $rel (sort sortfunc keys %relation) {
 	my @a=split(",",$rel);
