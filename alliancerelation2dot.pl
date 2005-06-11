08fea4d18654c9dc35542264c525dac45ce0a4ac
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2005-06-11 12:21:10 +0000

    more exact coloring with new info

--- alliancerelation2dot.pl
+++ alliancerelation2dot.pl
@@ -1,45 +1,54 @@
 #!/usr/bin/perl -w
 use strict;
-sub replacefunc($$) {my ($rel,$n)=@_;
+my $entries=150;
+
+sub huefunc($) {my ($pop)=@_;
+	my $green=0.3;
+	my $red=1;
+	my $enemybound=-3;
+	my $friendbound=2;
+	if($pop<$enemybound) {return $red}
+	if($pop>$friendbound) {return $green}
+	return ($red - (($pop-$enemybound)*($red-$green)/($friendbound-$enemybound))); # blue/purple
+}
+sub replacefunc($$$$) {my ($rel,$n,$conq1,$conq2)=@_;
 	my $l="";
-#	if($in>100) {}#$l=qq!,label="$in"!}
-#	else {$l=",color=gray"}
-	my $f=int(sqrt($rel/6));
-	my $fr=$rel/$n;
-	my %hue=();
-	for(0..9) {$hue{$_}=0.02}
-	for(13..30) {$hue{$_}=0.3}
-	for(10..12) {$hue{$_}=0.65}
+	my $fr=$n?$rel/$n:2; # average minimum pop
 	my $min=-0.1;
+	$fr-=3+$conq1+$conq2;
 	my $saturation=($n/11)*(1-$min)+$min;
 	$saturation=($saturation>1)?1:($saturation<0?0:$saturation);
-	my $c='"'.join(",",$hue{int($fr)},$saturation,1).'"';
-	return qq! [weight=$f,color=$c$l];!;
+	my $c='"'.join(",",huefunc($fr),$saturation,1).'"';
+	return qq! [color=$c$l];!;
 }
 
 print "graph alliances {\n";
-my $edges=`head -150 $ARGV[0]`;
+my $edges=`head -$entries $ARGV[0]`;
 my @edges=split("\n",$edges);
 my %allis;
 foreach(@edges) {
-	next unless /^([^ ]+) -- ([^ ]+) /;
+	next unless /^([^ ]+) -- ([^ ;]+)/;
 	$allis{$1}++;
 	$allis{$2}++;
 }
 sub max($$) {return $_[0]>$_[1]?$_[0]:$_[1]}
 sub sortfunc {
-	return 0 unless $a=~/^([^ ]+) -- ([^ ]+) /;
+	return 0 unless $a=~/^([^ ]+) -- ([^ ;]+)/;
 	my $max=max($allis{$1},$allis{$2});
-	return 0 unless $b=~/^([^ ]+) -- ([^ ]+) /;
+	return 0 unless $b=~/^([^ ]+) -- ([^ ;]+)/;
 	my $max2=max($allis{$1},$allis{$2});
 	return $max2<=>$max;
 }
 
 my @newedges;
 foreach(sort sortfunc @edges) {
-	s!; //(\d+) ([+-]?\d+)!replacefunc($1,$2)!e;
+	next unless m!^([^ ]+) -- ([^ ;]+); // (\d+) (\d+) (\d+) (\d+) ([+-]?\d+)!;
+	my ($a1,$a2,$rel,$n,$conq1,$conq2)=($1,$2,$3,$4,$5,$6);
+	#s!; //(\d+) ([+-]?\d+)!replacefunc($1,$2)!e;
+	$_="$a1 -- $a2". replacefunc($rel,$n,$conq1,$conq2);
 	push @newedges,$_;
 }
+#my $nodes=join " ",keys %allis;
 print join("\n",@newedges),"
 //overlap=scale;
 overlap=false;
