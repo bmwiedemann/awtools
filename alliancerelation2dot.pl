c39b9c3a4e03c6deae86cff390b790c97cbed1e5
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2005-06-18 03:57:41 +0000

    sort ourselves, clip ourselves

--- alliancerelation2dot.pl
+++ alliancerelation2dot.pl
@@ -1,6 +1,27 @@
 #!/usr/bin/perl -w
 use strict;
-my $entries=150;
+my $entries=160;
+my @groups=(
+#[qw{HNU HND GPS MAD}], 
+#[qw{LA LD FO SP}],
+#[qw{FUN STFU ION RATS DAT}],
+#[qw{WAR WHS TPQ}],
+#[qw{SSS XXX IB TGE}],
+#[qw{SpIn DUDE}],
+#[qw{FrS OINK wd}]
+#[qw{NEMO OMEN}],
+#[qw{THE NSA BKA PEYO}]
+);
+my %group;
+{
+ my $n=0;
+ foreach my $g (@groups) {
+	foreach (@$g) {
+		$group{$_}=$g;
+	}
+	unshift(@$g, ++$n);
+ }
+}
 
 sub huefunc($) {my ($pop)=@_;
 	my $green=0.3;
@@ -15,7 +36,8 @@ sub replacefunc($$$$) {my ($rel,$n,$conq1,$conq2)=@_;
 	my $l="";
 	my $fr=$n?$rel/$n:2; # average minimum pop
 	my $min=-0.1;
-	$fr-=3+$conq1+$conq2;
+	if(!$n) {$n=1}
+	$fr-=3+($conq1+$conq2)/$n**0.25;
 	my $saturation=($n/11)*(1-$min)+$min;
 	$saturation=($saturation>1)?1:($saturation<0?0:$saturation);
 	my $c='"'.join(",",huefunc($fr),$saturation,1).'"';
@@ -23,10 +45,14 @@ sub replacefunc($$$$) {my ($rel,$n,$conq1,$conq2)=@_;
 }
 
 print "graph alliances {\n";
-my $edges=`head -$entries $ARGV[0]`;
-my @edges=split("\n",$edges);
+open(F, "<", shift(@ARGV)) or die "error opening input file: $!";
+my @edges;
+{ local undef $/;
+  my $edges=<F>;
+  @edges=split("\n",$edges);
+}
 my %allis;
-foreach(@edges) {
+foreach(@edges[0..$entries-1]) {
 	next unless /^([^ ]+) -- ([^ ;]+)/;
 	$allis{$1}++;
 	$allis{$2}++;
@@ -41,15 +67,22 @@ sub sortfunc {
 }
 
 my @newedges;
-foreach(sort sortfunc @edges) {
+foreach(sort sortfunc @edges[0..$entries-1]) {
 	next unless m!^([^ ]+) -- ([^ ;]+); // (\d+) (\d+) (\d+) (\d+) ([+-]?\d+)!;
 	my ($a1,$a2,$rel,$n,$conq1,$conq2)=($1,$2,$3,$4,$5,$6);
 	#s!; //(\d+) ([+-]?\d+)!replacefunc($1,$2)!e;
+	for($a1,$a2) {
+		if($group{$_}) 
+			{$_=${$group{$_}}[0]}
+			#{$_=join "_",@{$group{$_}}}
+	}
+	next if $a1 eq $a2;
 	$_="$a1 -- $a2". replacefunc($rel,$n,$conq1,$conq2);
 	push @newedges,$_;
 }
 #my $nodes=join " ",keys %allis;
-print join("\n",@newedges),"
+print #"node [shape=plaintext,height=.1,width=0.1];".
+join("\n",@newedges),"
 //overlap=scale;
 overlap=false;
 splines=true;
