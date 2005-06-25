1c4e90e797b4b39cbe9a2974de67b04332b15ee1
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2005-06-25 05:57:39 +0000

    fix shadowed var

--- feed/news.pm
+++ feed/news.pm
@@ -6,22 +6,22 @@ my $name=$::options{name};
 {
 	require "input-mysql.pm";
 	local $_=$_;
-	my $pid=playername2id($name);
-	if(!$pid) {
+	my $dplayerid=playername2id($name);
+	if(!$dplayerid) {
 		print "error, unknown reporting player: '$name'\n";
 		return;
 	}
 	for(;(@a=m!<tr[^>]*><td[^>]*>([^>]*)</td><td[^>]*>Your attacking fleet was defeated by <a href=/0/Player/Profile.php/\?id=(\d+)>([^>]*)</a> [^>]*You killed about (\d+)%.</td></tr>(.*)!); $_=$a[4]) {
 		my ($time,$pid,$splayer,$amount)=@a;
 		$time=parseawdate($time);
-		print gmtime($time)." $pid $splayer-&gt;$name $amount%<br />\n";
 		$splayerid=playername2id($splayer);
+		print gmtime($time)." $pid $splayer($splayerid)-&gt;$name($dplayerid) $amount%<br />\n";
 		if(!$splayerid) {
 			print "unknown player $splayer\n";
 			next;
 		}
 		if(!$debug) {
-			dbtransferadd($time,$splayerid,$pid,$amount);
+			dbtransferadd($time,$splayerid,$dplayerid,$amount);
 		} else { print "not added".br}
 	}
 }
