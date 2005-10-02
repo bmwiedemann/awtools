fad566a928e57290ca79c5b2239d63ecf5c55bef
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2005-10-02 05:08:37 +0000

    nicer constants, adjust for gold2 changes

--- arrival.pm
+++ arrival.pm
@@ -4,18 +4,11 @@ use strict;
 
 # calculates and returns travel time for given options
 sub traveltime { my ($options)=@_;
-my $speed=1+0.19*$$options{racespeed};
-my ($c,$a)=(850/60,34);
-#my ($c,$a)=(14.04,33.97);
-#my ($c,$a)=(14.135,33.93);
-#my ($c,$a)=(14.248,34.13);
-my $e=90/100; # was (89/99);
-my $basetime=$c+sqrt(sqrt($$options{distance})+$$options{planet}*0.1)*$a;
-
-my $time=$basetime/$speed*$e**$$options{energy};
-if($$options{own}) {$time/=2}
-
-return $time;
+   my ($c,$a,$p,$e,$rs)=(850/60, 34, 0.1, 91/100, 0.19);
+   my $basetime=$c+sqrt(sqrt($$options{distance})+$$options{planet}*$p)*$a;
+   my $time=$basetime/(1+$rs*$$options{racespeed}) *$e**$$options{energy};
+   if($$options{own}) {$time/=2}
+   return $time;
 }
 
 1;
