659b86996b618e5e7fb43dd7373cf74e9369991e
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-01-02 07:44:03 +0000

    minor fixes

--- mangle/news.pm
+++ mangle/news.pm
@@ -1,5 +1,5 @@
 # link mail from
-s%(>You have new mail from )([^<]+)(.<br><a href)%"$1<a href=/0/Player/Profile.php?id=".playername2id($2).">$2</a>$3"%ge;
+s%(>You have new mail from )([^<]+)(.<br><a href)%"$1<a href=/0/Player/Profile.php/?id=".playername2id($2).">$2</a>$3"%ge;
 
 require "mangle/special_color_incomings.pm";
 
--- mangle/special_color.pm
+++ mangle/special_color.pm
@@ -1,17 +1,17 @@
-sub manglecolor($) { my($name)=@_;
+sub manglecolor($$) { my($id,$name)=@_;
    my $col="white";
    my @rel=getrelation($name);
-   my $atag="";
+   my $alli="";
+   my $atag=playerid2tag($id);
+   if($atag) {$alli="[$atag] "}
+   elsif($rel[1]) {$alli="[$rel[1]] "}
    if($rel[0]) {
       $col=getrelationcolor($rel[0]);
    }
-   if($rel[1]) {
-      $atag=" [$rel[1]]";
-   }
-   return $col."\">$name$atag";
+   return $col."\">$alli$name";
 }
 
 # colorize player links
-   s%(<a href=/0/Player/Profile.php/?\?id=)(\d+)>([^<]*)</a>%$1.$2." style=\"color:".manglecolor($3)."</a>"%ge;
+   s%(<a href=/0/Player/Profile.php/?\?id=)(\d+)>([^<]*)</a>%$1.$2." style=\"color:".manglecolor($2,$3)."</a>"%ge;
 
 1;
