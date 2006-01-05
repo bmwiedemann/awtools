bd99b75e97733cf78c84e1042e1482f46f6a04aa
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-01-05 05:39:44 +0000

    add fleet source and launch button

--- mangle/fleet_send.pm
+++ mangle/fleet_send.pm
@@ -1,7 +1,7 @@
 use strict;
 use CGI;
 
-{
+if($::options{post}) {
    my $cgi=new CGI($::options{post});
 # add echo of fleet
    my @fleet;
@@ -15,11 +15,23 @@ use CGI;
 # add echo of destination
    my $destsid=$cgi->param("destination");
    my $destpid=$cgi->param("planet");
+   my $srcsid=$cgi->param("nr");
+   my $srcpid=$cgi->param("id");
    my $destname=systemid2name($destsid)||"";
+   my $srcname=systemid2name($srcsid)||"";
 # add current time - but no timezone known so only GMT
    my $time=AWisodatetime(time());
    
-   s/<b>Calculated/Fleet: $fleet<br>Target: $destsid#$destpid = $destname #$destpid<br>Launch time: $time GMT<br>$&/;
+   s/<b>Calculated/Fleet: $fleet<br>From: $srcsid#$srcpid = $srcname #$srcpid<br>To: $destsid#$destpid = $destname #$destpid<br>Launch time: $time GMT<br>$&/;
+
+# add submit to send
+   my $form="<form method=\"post\">";
+   foreach my $p ($cgi->param) {
+      next if $p eq "calc";
+      $form.="\n<input type=\"hidden\" name=\"$p\" value=\"".($cgi->param($p)).'">';
+   }
+   $form.="<input type=\"submit\" value=\"Launch !!!\" class=smbutton></form>";
+   s%</small>% $& or click $form%;
 #   $_.="post: ".$::options{post};
 }
 
