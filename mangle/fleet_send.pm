67554c150d5e21da6bf1464ad7d1bd139cc9d9e9
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-01-06 05:10:25 +0000

    add a lot of information

--- mangle/fleet_send.pm
+++ mangle/fleet_send.pm
@@ -1,5 +1,6 @@
 use strict;
 use CGI;
+use awstandard;
 
 if($::options{post}) {
    my $cgi=new CGI($::options{post});
@@ -12,6 +13,30 @@ if($::options{post}) {
       push(@fleet, "$n $s");
    }
    my $fleet=join(", ",@fleet);
+   
+# add current time (UTC/GMT)
+   my $time=time();
+
+# calc timezone
+   my $extrainfo="";
+   my $tz;
+   if(m/<title>(.*)/) {
+      $tz=awstandard::guesstimezone($1);
+      $extrainfo.="Your timezone: UTC+$tz s<br>";
+   }
+   my $extrainfo2="";
+   if(m/Calculated arrival time: ([^<]*)/) {
+      my $altime=parseawdate($1);
+      my $suf="L";
+      my $flighttime="";
+      if(defined $tz) { # convert to UTC
+         $altime-=$tz;
+         $suf="UTC";
+         my $t=$altime-$time;
+         $flighttime=sprintf("Flight time: %is = %.2fh = %i:%.2i:%.2i<br>", $t, $t/3600, $t/3600, $t/60%60, $t%60);
+      }
+      $extrainfo2.="Arrival time: ".AWisodatetime($altime)." $suf <br>$flighttime";
+   }
 # add echo of destination
    my $destsid=$cgi->param("destination");
    my $destpid=$cgi->param("planet");
@@ -19,10 +44,9 @@ if($::options{post}) {
    my $srcpid=$cgi->param("id");
    my $destname=systemid2name($destsid)||"";
    my $srcname=systemid2name($srcsid)||"";
-# add current time - but no timezone known so only GMT
-   my $time=AWisodatetime(time());
+   $time=AWisodatetime($time);
    
-   s/<b>Calculated/Fleet: $fleet<br>From: $srcsid#$srcpid = $srcname #$srcpid<br>To: $destsid#$destpid = $destname #$destpid<br>Launch time: $time GMT<br>$&/;
+   s/<b>Calculated/Fleet: $fleet<br>From: $srcsid#$srcpid = $srcname #$srcpid<br>To: $destsid#$destpid = $destname #$destpid<br>${extrainfo}Launch time: $time UTC<br>$extrainfo2$&/;
 
 # add submit to send
    my $form="<form method=\"post\">";
