330c0f1c0d434bee304d4ebaac7022680cf41c4b
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-01-08 04:39:51 +0000

    fix bug

--- mangle/planets_detail.pm
+++ mangle/planets_detail.pm
@@ -8,14 +8,13 @@ m%Production Points</a></td><td>\s*(\d+)</td>%;
 my $pp=$1;
 
 
-my $n=0;
-foreach my $buil (@buildings) {
+foreach my $n (0..$#buildings) {
+   my $buil=$buildings[$n];
    next if(! m%($buil</a></td><td>)(\d+)(.*?\n<td> *)(\d+)(</td></tr>)%);
    my ($level,$ppneeded)=($2,$4);
    next if($ppneeded>1000 || $ppneeded>$pp);
    s%($buil</a></td><td>)(\d+)(.*?\n<td> *)(\d+)(</td></tr>)%$1$2$3$4 <a href="/0/Planets/Spend_Points.php/?p=$pp&amp;i=$planet&amp;points=$4&amp;produktion=$val[$n]" style="background-color:red">+1</a>$5%;
 #   $debug.="<br>test: $buil $val[$n] $2 $4";
-   $n++;
 }
 
 #$_.=$debug;
