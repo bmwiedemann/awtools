28d24c502084dcd4e8574a4016ecbe37e59b9eaf
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-01-10 20:29:52 +0000

    greatly improve things

--- mangle/dispatch.pm
+++ mangle/dispatch.pm
@@ -7,15 +7,19 @@ use DBAccess;
 $::bmwlink="<a href=\"http://$bmwserver/cgi-bin";
 
 sub manglefilter { 
+   my %info=("alli"=>$ENV{REMOTE_USER}, "user"=>$::options{name});
    my $gameuri=defined($::options{url}) && $::options{url}=~m%^http://www1\.astrowars\.com/%;
    my $ingameuri=$gameuri && $::options{url}=~m%^http://www1\.astrowars\.com/0/%;
    my $title="";
    my $module="";
    my $alli="\U$ENV{REMOTE_USER}";
    
-   if($gameuri && $::options{name} && $::options{url}=~m%^http://www1.astrowars.com/register/login.php% #&& (my $session=${$::options{headers}}{Cookie})) { # reset click counter now
-      ) {
-         $dbh->do("DELETE FROM `usersession` WHERE name = ".$dbh->quote($::options{name}));
+   if($gameuri && $::options{name} && $::options{url}=~m%^http://www1.astrowars.com/register/login.php% && (my $session=${$::options{headers}}{Cookie})) { # reset click counter now
+         $session=~s/^.*PHPSESSID=([^; ]*).*/$1/;
+         $dbh->do("UPDATE `usersession` SET `nclick` = '0' WHERE `sessionid` = ".$dbh->quote($session));
+   }
+   if($::options{url}=~m%^http://www\.astrowars\.com/about/battlecalculator%) {
+      s/(form action="" method=")post/$1get/;
    }
    if($gameuri && m&<title>([^<]*)</title>&) {
       $title=$1;
@@ -24,35 +28,35 @@ sub manglefilter {
       if(-e $include) {
          do $include;
          if($@) {$module="error in $module: $@";}
-         else { $module="mangling applied: $module"; # for the log
+         else { $module="filtered $module"; # for the log
          }
       }
-      else {$module="no special mangling for: $module"}
-      $module=qq'<p style="color:gray">$module</p>';
+      else {$module="$module"}
+#      $module="($module)"; #qq'<span style="color:gray">($module)</span>';
+      $info{page}=$module;
 
 # add main AWTool link
       if(1 && $ingameuri && (my $session=${$::options{headers}}{Cookie})) {
-         $session=~s/^.*PHPSESSID=([^; ]*).*/$1/;
          my $nclicks="";
-         if(1) {
-            my %sessions;
-            tie %sessions,'Tie::DBI',$dbh, 'usersession', 'sessionid',{CLOBBER=>1};
-            my $s=$sessions{$session};
-            if(!$s) {
-               $s={"sessionid"=>$session, "name"=>$::options{name}, "nclick"=>0, "firstclick"=>time()};
+         if($session=~s/^.*PHPSESSID=([a-f0-9]{32}).*/$1/) {
+            my $time=time();
+            my $result=$dbh->do("UPDATE `usersession` SET `nclick` = `nclick` + 1 , `lastclick` = '$time' WHERE `sessionid` = '$session' LIMIT 1;");
+            if($result ne "0E0") {
+               my $ref=$dbh->selectall_arrayref("SELECT `nclick` FROM `usersession` WHERE `sessionid` = '$session';");
+               $nclicks=$$ref[0][0];
+            } else { #insert
+              $nclicks=0;
+              $dbh->do("INSERT INTO `usersession` VALUES ( '$session', '$::options{name}', '0', '$time', '$time');");
             }
-            $nclicks=$$s{nclick}+1;
-            $$s{nclick}=$nclicks;
-            $$s{lastclick}=time();
-            $sessions{$session}=$s;
             if($nclicks>290) {$nclicks=qq'<b style="color:#f44">$nclicks</b>'}
+            $info{clicks}=$nclicks;
          }
-         s%Fleet</a></td>%$&<td>|</td><td>$::bmwlink/index.html">AWTools</a> $nclicks</td>%;
-#         $::bmwlink/authaw?session=$session">AWTools...
+#s%Fleet</a></td>%$&<td>|</td><td>$::bmwlink/index.html">AWTools</a> $nclicks</td>%;
+         s%Fleet</a></td>%$&<td>|</td><td>$::bmwlink/authaw?session=$session">AWTools</a></td>%;
       }
 
 # colorize player links
-      require "mangle/color.pm"; mangle_player_color();
+      require "mangle/special_color.pm"; mangle_player_color();
 
    }
 
@@ -60,12 +64,14 @@ sub manglefilter {
    s/<table><tr><td><table bgcolor="#\d+" style="cursor: pointer;".*//;
 # disable ad
    s/(?:pagead2\.googlesyndication\.com)|(?:games\.advertbox\.com)|(?:oz\.valueclick\.com)|(?:optimize\.doubleclick\.net)/localhost/g;
+#   s%<br>\s*(<TABLE)%$1%; # remove some blanks
 
-# add disclaimer
+# add footer + disclaimer
    if(!$alli) {$alli=qq!<b style="color:red">no</b>!}
-   s%</body>%</center>disclaimer: this page was mangled by greenbird's code (for $::options{name} in context of $alli alliance data). <br>This means that errors in display or functionality might not exist in the original page. <br>If you are unsure, disable mangling and try again. $module $&%;
+   my $info=join(" ", map({"<span style=\"color:gray\">$_=</span>$info{$_}"} sort keys %info));
+   my $gbcontent="<p style=\"text-align:center; color:white; background-color:black\">disclaimer: this page was mangled by greenbird's code. <br>This means that errors in display or functionality might not exist in the original page. <br>If you are unsure, disable mangling and try again.<br>$info</p>";
+   s%</body>%</center>$gbcontent $&%;
 
-   s%<br>\s*(<TABLE)%$1%;
 }
 
 sub mangle_dispatch(%) { my($options)=@_;
