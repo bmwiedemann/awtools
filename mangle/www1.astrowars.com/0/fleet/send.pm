2ae7b942f0c9b7954c3be1b9cddb3f80f9ea66f6
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-01-28 17:25:18 +0000

    working javascript for updating ETA

--- mangle/www1.astrowars.com/0/fleet/send.pm
+++ mangle/www1.astrowars.com/0/fleet/send.pm
@@ -5,6 +5,13 @@ use awstandard;
 my $align=' align="right" style="padding:0px; color:#4978ff"';
 my $delim=": &nbsp;";
 
+sub addtimer($$$) { my($n,$time,$script)=@_;
+   my($sec,$min,$hour,$mday,$mon,$year)=gmtime($time);
+   $mon++;$year+=1900;
+   $script=~s% //inserthere%$&\n e[$n] = [$year, $mon, $mday, $hour,$min,$sec];c$n=window.setInterval("C($n)",1000);%;
+   $_[2]=$script;
+}
+
 if($::options{post}) {
    my $cgi=new CGI($::options{post});
 # add echo of fleet
@@ -19,6 +26,7 @@ if($::options{post}) {
    
 # add current time (UTC/GMT)
    my $time=time();
+   my $altime;
 
 # calc timezone
    my $extrainfo="";
@@ -27,11 +35,11 @@ if($::options{post}) {
       $tz=awstandard::guesstimezone($1);
       my $tzh=sprintf("%i",$tz/3600);
       if($tz>=0) {$tzh="+$tzh"}
-      $extrainfo.="<tr><td$align>Your timezone$delim</td><td> UTC$tzh </td></tr>";
+      $extrainfo.="<tr><td$align>Timezone$delim</td><td> UTC$tzh </td></tr>";
    }
    my $extrainfo2="";
    if(m/Calculated arrival time: ([^<]*)/) {
-      my $altime=parseawdate($1);
+      $altime=parseawdate($1);
       my $suf="L";
       my $flighttime="";
       if(defined $tz) { # convert to UTC
@@ -40,8 +48,50 @@ if($::options{post}) {
          my $t=$altime-$time;
          $flighttime=sprintf("<tr><td$align>Flight time$delim</td><td> %is = %.2fh = %i:%.2i:%.2i</td></tr>", $t, $t/3600, $t/3600, $t/60%60, $t%60);
       }
-      $extrainfo2.="<tr><td$align>Arrival time$delim</td><td> ".AWisodatetime($altime)." $suf </td></tr>$flighttime";
+      $extrainfo2.="<tr><td$align>Arrival time$delim</td><td> <!--inserthere-->".AWisodatetime($altime)." $suf </td></tr>$flighttime";
    }
+
+   my $script="";
+
+# auto-update arrival time
+if(1) {
+   $script=qq^
+<script type="text/javascript">
+<!--
+var e = new Array();
+var maxe = new Array();
+maxe = [9999, 12, 30, 24, 60, 60];
+   //inserthere
+
+function C(k) {
+  var i=5;
+  e[k][i]++;
+  while(i>=1) {
+     if (e[k][i]==maxe[i]) {
+       e[k][i-1]++;
+       e[k][i]=0;
+     } else { break }
+     i--;
+  }
+  
+  document.forms[k].z.value=e[k][0]+'-'+(e[k][1]>9?e[k][1]:'0'+e[k][1])+'-'+(e[k][2]>9?e[k][2]:'0'+e[k][2])+' '+(e[k][3]>9 ? e[k][3] : '0'+e[k][3])+':'+(e[k][4]>9 ? e[k][4] : '0'+e[k][4])+':'+(e[k][5]>9?e[k][5]:'0'+e[k][5]);
+}
+      //-->
+   </script>
+      ^;
+   if($altime) {
+      addtimer(1, $altime, $script);
+      $extrainfo2=~s%<!--inserthere-->([^<]*)%<form><input type="text" name="z" class="text" style="text-align:left; background-color: #000;" value="$altime">$1</form>%;
+   }
+   addtimer(0, $time, $script);
+   $time=AWisodatetime($time);
+   $time=qq'<form><input type="text" name="z" class="text" style="text-align:left; background-color: #000;" value="$time"> $time UTC</form>';
+   s%</head>%<script type="text/javascript" src="http://aw.lsmod.de/code/js/fleet_send.js"></script>$&%;
+} else {
+   $time=AWisodatetime($time);
+}
+
+
 # add echo of destination
    my $destsid=$cgi->param("destination");
    my $destpid=$cgi->param("planet");
@@ -49,15 +99,14 @@ if($::options{post}) {
    my $srcpid=$cgi->param("id");
    my $destname=display_sid2($destsid);
    my $srcname=display_sid2($srcsid);
-   $time=AWisodatetime($time);
-
+   
 # add awauth
    for my $l ($destname, $srcname) {
       $l=~s%<a href="http://aw.lsmod.de/cgi-bin%$::bmwlink%;
    }
-   
+
 # add everything only here to the output HTML:
-   s%(<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#000000' width="600"><tr><td><br>\n)(<b>)%$1 <table><tr><td$align>Fleet$delim</td><td> $fleet</td></tr><tr><td$align>From$delim</td><td> $srcsid#$srcpid = $srcname $srcpid</td></tr><tr><td$align>To$delim</td><td> $destsid#$destpid = $destname $destpid</td></tr>${extrainfo}<tr><td$align>Launch time$delim</td><td> $time UTC</td></tr>$extrainfo2</table><br> $2%;
+   s%(<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#000000' width="600"><tr><td><br>\n)(<b>)%$1 <table><tr><td$align>Fleet$delim</td><td> $fleet</td></tr><tr><td$align>From$delim</td><td> $srcsid#$srcpid = $srcname $srcpid</td></tr><tr><td$align>To$delim</td><td> $destsid#$destpid = $destname $destpid</td></tr>${extrainfo}<tr><td$align>Launch time$delim</td><td> $time</td></tr>$extrainfo2</table>$script<br> $2%;
 #   s%<b>Calculated%<table><tr><td$align>Fleet$delim</td><td> $fleet</td></tr><tr><td$align>From$delim</td><td> $srcsid#$srcpid = $srcname $srcpid</td></tr><tr><td$align>To$delim</td><td> $destsid#$destpid = $destname $destpid</td></tr>${extrainfo}<tr><td$align>Launch time$delim</td><td> $time UTC</td></tr>$extrainfo2</table>$&%;
 
 # add submit to send
