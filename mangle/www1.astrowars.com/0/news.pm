07abb7442ad86dafdffa9ae99e1adfdc50b7476d
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-01-29 10:00:01 +0000

    add system-link to events

--- mangle/www1.astrowars.com/0/news.pm
+++ mangle/www1.astrowars.com/0/news.pm
@@ -1,6 +1,11 @@
 # link mail from
 s%(>You have new mail from )([^<]+)(.<br><a href)%"$1<a href=/0/Player/Profile.php/?id=".playername2id($2).">$2</a>$3"%ge;
 
+# add links to systems
+s%((?:We just colonized|We conquered|We lost|The people are not willing to take over) )([^.!]+)( \d+)[.!]%$1$::bmwlink/system-info?name=$2">$2$3</a>.%g;
+s%(Congratulations! (?:Your attacking fleet was|We were) victorious at )([^.]+)( \d+)\.%$1$::bmwlink/system-info?name=$2">$2$3</a>.%g;
+s%(Your (?:attack|defend)ing fleet was defeated by <a href=[^<]*</a> at )([^.]+)( \d+)(\. You killed about \d+\%\.)%$1$::bmwlink/system-info?name=$2">$2$3</a>$4%;
+
 require "mangle/special_color_incomings.pm"; mangle_incoming();
 
 1;
