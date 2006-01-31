e26abeef7344e07157c5e6411cd9a9b06d5815f0
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-01-31 19:19:23 +0000

    add remap

--- awaccess.pm
+++ awaccess.pm
@@ -3,7 +3,7 @@ require Exporter;
 use vars qw(@ISA @EXPORT);
 
 our (%read_access,%write_access);
-our %allowedalli=("af"=>1, "tgd"=>1, "xr"=>1, "love"=>1, "kk"=>1, "wink"=>1, "ocb"=>1, "fsb"=>1, ""=>0);
+our %allowedalli=("af"=>1, "tgd"=>1, "xr"=>1, "love"=>1, "kk"=>1, "wink"=>1, "ocb"=>1, "fsb"=>1, "frot"=>1, "esb"=>1, ""=>0);
 
 @ISA = qw(Exporter);
 @EXPORT = qw(%read_access %write_access %allowedalli);
@@ -13,5 +13,6 @@ $read_access{af}=[qw"af"];
 #$read_access{is}=[qw(is fun af)];
 #$read_access{tgd}=[qw(af)];
 $write_access{af}="af"; # unused yet
+our %remap_alli=(es=>"esb");
 
 1;
