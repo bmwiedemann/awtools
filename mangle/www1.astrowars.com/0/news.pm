e6487c7e170e4950aad5de50d60176605e100f79
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-02-11 21:25:54 +0000

    minor fix

--- mangle/www1.astrowars.com/0/news.pm
+++ mangle/www1.astrowars.com/0/news.pm
@@ -4,7 +4,7 @@ s%(>You have new mail from )([^<]+)(.<br><a href)%"$1<a href=/0/Player/Profile.p
 # add links to systems
 s%((?:We just colonized|We conquered|We lost|The people are not willing to take over) )([^.!]+)( \d+)[.!]%$1$::bmwlink/system-info?name=$2">$2</a>$3.%g;
 s%(Congratulations! (?:Your attacking fleet was|We were) victorious at )([^.]+)( \d+)\.%$1$::bmwlink/system-info?name=$2">$2</a>$3.%g;
-s%(Your (?:attack|defend)ing fleet was defeated by <a href=[^<]*</a> at )([^.]+)( \d+)(\. You killed about \d+\%\.)%$1$::bmwlink/system-info?name=$2">$2</a>$3$4%;
+s%(Your (?:attack|defend)ing fleet was defeated by <a href=[^<]*</a> at )([^.]+)( \d+)(\. You killed about \d+\%\.)%$1$::bmwlink/system-info?name=$2">$2</a>$3$4%g;
 
 require "mangle/special_color_incomings.pm"; mangle::special_color_incomings::mangle_incoming();
 
