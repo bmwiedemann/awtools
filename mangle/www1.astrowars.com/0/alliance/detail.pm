b4d26e55ffc80edea5ed3b95b593162bae999011
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-02-12 14:07:49 +0000

    fix next link conditional

--- mangle/www1.astrowars.com/0/alliance/detail.pm
+++ mangle/www1.astrowars.com/0/alliance/detail.pm
@@ -9,41 +9,40 @@ if($::options{url}=~/id=(\d+)/) {
    my $id=$1;
    my $url=$::options{url};
    $url=~s/(id=)\d+/$1/;
-   my $previd=$id-1;
-   my $nextid=$id+1;
-   my $prevstring="";
 
-   s%([^<>]*)(<br><table border=0)%
-      my $id=playername2id($1);
-      my $x=$1;
-      if($id) { $x="<a href=\"/0/Player/Profile.php/?id=$id\">$1</a>"; }
-      $x.$2%e;
-   if($previd>=0) { $prevstring=qq'<a href="$url$previd">prev</a>'; }
+   while(1) { # pseudo loop to exit at several places
+      my $user=$::options{name};
+      my $pid=playername2id($user);
+      last unless($pid && $pid>2);
+      my $aid=lc(playerid2alliance($pid));
+      last unless($aid);
+      my $members=awinput::allianceid2membersr($aid);
 
-   my $form="";
-   if(1) {#$::options{name} eq "greenbird") {
-# TODO: find alliance for name, #members, ranking order
-      while(1) { # pseudo loop to exit at several places
-         my $user=$::options{name};
-         my $pid=playername2id($user);
-         last unless($pid && $pid>2);
-         my $aid=lc(playerid2alliance($pid));
-         last unless($aid);
-         my $members=awinput::allianceid2membersr($aid);
-         my $n=0;
-         $form.=' <form><select name="id" onchange="submit()">';
-         foreach my $m (@$members) {
-            my $name=playerid2name($m);
-            my $sel=($id == $n)?" selected":"";
-            $form.=qq' <option value="$n"$sel>$name</option>';
-            $n++;
-         }
-         $form.='</select><input type="submit" class="smbutton" value="Go"></form> ';
-         last;
+      my $previd=$id-1;
+      my $nextid=$id+1;
+      my $prevstring="";
+
+      s%([^<>]*)(<br><table border=0)%
+         my $id=playername2id($1);
+         my $x=$1;
+         if($id) { $x="<a href=\"/0/Player/Profile.php/?id=$id\">$1</a>"; }
+         $x.$2%e;
+      if($previd>=0) { $prevstring.=qq'<a href="$url$previd">prev</a>'; }
+      if($nextid<@$members) { $prevstring.=qq' <a href="$url$nextid">next</a>'; }
+      my $form="";
+      my $n=0;
+      $form.=' <form><select style="text-align:left;" name="id" onchange="submit()">';
+      foreach my $m (@$members) {
+         my $name=playerid2name($m);
+         my $sel=($id == $n)?" selected":"";
+         $form.=qq' <option value="$n"$sel>$name</option>';
+         $n++;
       }
-   }
+      $form.='</select><input type="submit" class="smbutton" value="Go"></form> ';
    
-   s%<br><table border=0%<br>$prevstring <a href="$url$nextid">next</a>$form$&%;
+      s%<br><table border=0%<br>$prevstring$form$&%;
+      last;
+   }
    do "mangle/www1.astrowars.com/0/alliance.pm";
 }
 
