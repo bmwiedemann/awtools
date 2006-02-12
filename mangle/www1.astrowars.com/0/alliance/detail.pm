ff92094075b042a02e0d91940a6d83e57bd47864
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-02-12 09:11:56 +0000

    add drop-down list

--- mangle/www1.astrowars.com/0/alliance/detail.pm
+++ mangle/www1.astrowars.com/0/alliance/detail.pm
@@ -16,10 +16,34 @@ if($::options{url}=~/id=(\d+)/) {
    s%([^<>]*)(<br><table border=0)%
       my $id=playername2id($1);
       my $x=$1;
-      if($id) { $x="<a class=\"awtools\" href=\"/0/Player/Profile.php/?id=$id\">$1</a>"; }
+      if($id) { $x="<a href=\"/0/Player/Profile.php/?id=$id\">$1</a>"; }
       $x.$2%e;
-   if($previd>=0) { $prevstring=qq'<a class="awtools" href="$url$previd">prev</a>'; }
-   s%<br><table border=0%<br>$prevstring <a class="awtools" href="$url$nextid">next</a>$&%;
+   if($previd>=0) { $prevstring=qq'<a href="$url$previd">prev</a>'; }
+
+   my $form="";
+   if(1) {#$::options{name} eq "greenbird") {
+# TODO: find alliance for name, #members, ranking order
+      while(1) { # pseudo loop to exit at several places
+         my $user=$::options{name};
+         my $pid=playername2id($user);
+         last unless($pid && $pid>2);
+         my $aid=lc(playerid2alliance($pid));
+         last unless($aid);
+         my $members=awinput::allianceid2membersr($aid);
+         my $n=0;
+         $form.=' <form><select name="id" onchange="submit()">';
+         foreach my $m (@$members) {
+            my $name=playerid2name($m);
+            my $sel=($id == $n)?" selected":"";
+            $form.=qq' <option value="$n"$sel>$name</option>';
+            $n++;
+         }
+         $form.='</select><input type="submit" class="smbutton" value="Go"></form> ';
+         last;
+      }
+   }
+   
+   s%<br><table border=0%<br>$prevstring <a href="$url$nextid">next</a>$form$&%;
    do "mangle/www1.astrowars.com/0/alliance.pm";
 }
 
