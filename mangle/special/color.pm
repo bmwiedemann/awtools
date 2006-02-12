b90421a3fbf62a9fa18899082074798c9152be0a
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-02-12 09:10:53 +0000

    cleanup

---
+++ mangle/special/color.pm
@@ -0,0 +1,25 @@
+package mangle::special::color;
+use awstandard;
+use awinput;
+
+sub manglecolor($$) { my($id,$name)=@_;
+   my $col="white";
+   my @rel=getrelation($name);
+   my $alli="";
+   my $atag=playerid2tag($id);
+   if($atag) {$alli="[$atag] "}
+   elsif($rel[1] && $id>2) {$alli="[$rel[1]] "}
+   if($rel[0]) {
+      $col=getrelationcolor($rel[0]);
+   }
+   return $col."\">$alli$name";
+}
+
+sub mangle() {
+# colorize player links
+   s%(<a href=/0/Player/Profile\.php/?\?id=)(\d+)>([^<]*)</a>%$1.$2." style=\"color:".manglecolor($2,$3)."</a>"%ge;
+   s%(<a href="profile\.php\?mode=viewprofile&amp;u=)(\d+)("[^>]*)>([^<]*)</a>%$1.$2.$3." style=\"color:".manglecolor($2,$4)."</a>"%ge;
+   s%<a href\s*=("?)/0/Glossary/%<a class="awglossary" href=$1/0/Glossary/%g;
+}
+
+1;
---
+++ mangle/special/dispatch.pm
@@ -0,0 +1,19 @@
+package mangle::special;
+
+sub mangle() {
+   # modify title to work better with tabs
+   s/^\s*(<html><head><title>) Astro Wars /$1/;
+   
+   if($ENV{REMOTE_USER}) {
+   # remove text ads
+      s/<table><tr><td><table bgcolor="#\d+" style="cursor: pointer;".*//;
+   # disable other ads
+      s/(?:pagead2\.googlesyndication\.com)|(?:games\.advertbox\.com)|(?:oz\.valueclick\.com)|(?:optimize\.doubleclick\.net)/localhost/g;
+   }
+   # fix color specification
+   s%bgcolor="([0-9a-fA-F]{6})"%bgcolor="#$1"%g;
+   # add icon
+   s%</head>%<link type="image/vnd.microsoft.icon" rel="icon" href="http://aw.lsmod.de/awfavicon.ico">\n<link rel="shortcut icon" href="http://aw.lsmod.de/awfavicon.ico">\n</head>%;
+}
+
+1;
