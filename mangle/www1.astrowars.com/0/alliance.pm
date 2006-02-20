341e2f16512969f145417cd937e5f6793916a48b
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-02-20 18:04:57 +0000

    add direct links

--- mangle/www1.astrowars.com/0/alliance.pm
+++ mangle/www1.astrowars.com/0/alliance.pm
@@ -2,4 +2,11 @@
 my $alli=$ENV{REMOTE_USER};
 s%<td><a href=/0/Alliance/>Overview</a></td>%<td>$::bmwlink/alliance?alliance=$alli">AWtools($alli)</a></td><td>|</td> $&%;
 
+
+# add direct links for details pages
+if($::options{url}=~m%/0/Alliance/$%) {
+   my $n=0;
+   s%(<td>)(\d+)(</td><td><a href=http://www.astrowars.com/forums/privmsg.php)%$1.'<a href="Detail.php/?id='.$n++.'">'.$2."</a>".$3%ge;
+}
+
 1;
