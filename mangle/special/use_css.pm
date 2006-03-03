5018460862df598ea9d7faed499d6c4d7a047455
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2006-03-03 06:14:45 +0000

    fix bottom navi detector

--- mangle/special/use_css.pm
+++ mangle/special/use_css.pm
@@ -11,7 +11,7 @@ if($::options{url}=~/www1\.astrowars\.com\/0/) {
    s%<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#404040' width="600">\n<tr height=15 align="center"><td width="140" bgcolor="#202060">%<table class="top_navi">\n<tr class="t_navi_links"><td class="t_navi_title">%;
    s%<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#000000' width="600">%<table class="main_outer">%;
    s%<TABLE BORDER="0" CELLSPACING="1" CELLPADDING="1" bgcolor='#000000' width="600">%<table class="main_inner" cellspacing="1">%;
-   s%<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#404040' width="600">%<table class="bottom_navi">%;
+   s%<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#404040' width="600">(\s+<tr height=15)%<table class="bottom_navi">$1 class="t_navi_links"%;
    
    my $c=qr/([34]0)\1\1/;
    s%<tr align=center bgcolor="?#$c"? onMouseOver=this.style.backgroundColor="#206060"   onMouseOut=this.style.backgroundColor="#$c"%<tr class="mouseout$1" onMouseOver="this.className='mousein$1'"   onMouseOut="this.className='mouseout$1'"%go;
