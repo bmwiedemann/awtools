19d1d4c690306ee1c65ff856ffaf0844bca33ea8
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2007-02-17 08:58:29 +0000

    working image caching

--- awcache.pm
+++ awcache.pm
@@ -3,36 +3,97 @@
 package awcache;
 use strict;
 use warnings;
+use Time::Local;
+use awstandard;
 
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT =
 qw(&awservecache);
 
+my %month;
+foreach my $m (0..11) {
+	$month{$awstandard::month[$m]}=$m;
+}
+my %mimemap=qw(
+gif image/gif
+png image/png
+css text/css
+htm.? text/html
+txt text/plain
+);
+
+my $cachedir="/home/aw/html/awcache";
+
+
+# input: HTTP time format string
+# output: integer seconds since epoch (1970-01-01)
+sub HTTPtoEpoch($) {
+	my($mday,$mon,$year,$hour,$min,$sec)=($_[0]=~m/^\w{3}, (\d+) (\w{3}) (\d+) (\d\d):(\d\d):(\d\d)/);
+	return timegm($sec,$min,$hour,$mday,$month{$mon},$year);
+}
+
 # input: string of pathname to use as cache
 # output: content of file
-sub awservecache($)
+sub awgetcache($)
 { my($path)=@_;
 #   my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)=stat($path);
+	my $content;
+	my $mtime;
    my $res=open(FCACHE, "<", $path);
-   if($res) {
-      local $/;
-      $res=<FCACHE>;
-      close FCACHE;
-      return $res;
-   }
+   if(!$res) {
+		# re-fetch now
+		print STDERR "re-fetching $path\n";
+		my $request=$::options{request};
+		my $UA=$::options{ua};
+		my $response = $UA->request($request);
+		$content = $response->content;
+		(my $dir=$path)=~s![^/]*$!!;
+		system("/bin/mkdir", "-p", $dir);
+		open(my $fd, ">", $path);
+		syswrite($fd, $content);
+		close $fd;
+			
+		$response->scan(sub {
+			if(lc($_[0]) eq "last-modified") {
+				$mtime=HTTPtoEpoch($_[1]);
+			}
+		});
 
-   # re-fetch now
-   my $req=$::options{request};
-   my $UA=$::options{ua};
-   my $response = $UA->request($req);
-   my $content = $response->content;
-   (my $dir=$path)=~s![^/]*$!!;
-   system("/bin/mkdir", "-p", $dir);
-   open(my $fd, ">", $path);
-   syswrite($fd, $content);
-   close $fd;
-   return $content;
+		utime(time(),$mtime,$path);
+	} else {
+      local $/;
+      $content=<FCACHE>;
+      close FCACHE;
+		$mtime=(stat($path))[9];
+   }
+	my $t;
+	foreach my $k (keys(%mimemap)) {
+		if($path=~m/\.$k$/) {
+			$t=$mimemap{$k};
+		}
+   }
+   return ($content,$mtime,$t);
+}
+
+sub awservecache()
+{
+	my $u=$::options{url};
+	$u=~s!http://!$cachedir/!;
+#	print STDERR $u." url \n";
+	my ($c,$mtime,$mimetype)=awgetcache($u);
+	if($c) {
+		my $r=$::options{req}; # apache request obj
+		$r->status(200);
+		$r->content_type($mimetype);
+		$r->set_content_length(length($c));
+		$r->set_last_modified($mtime);
+		$r->header_out('Expires', HTTP::Date::time2str(time + 90*24*60*60)); # 90d
+		$r->set_keepalive();
+		$r->print($c);
+		return 1;
+	}
+	return 2;
 }
 
 1;
--- preproc/www1.astrowars.com/images.pm
+++ preproc/www1.astrowars.com/images.pm
@@ -1,21 +1,4 @@
 use awcache;
 
-my $u=$::options{url};
-my $cachedir="/home/aw/html/awcache";
-$u=~s!http://!$cachedir/!;
-
-return 2;
-my $c=awservecache($u);
-if($c) {
-   my $r=$::options{req}; # apache request obj
-   my $t;
-   if($u=~m/\.gif$/) {
-      $t="image/gif";
-   }
-   $r->content_type($t);
-   $r->set_keepalive();
-   $r->print($c);
-   return 1;
-}
-
-2;
+#return 2;
+awservecache();
