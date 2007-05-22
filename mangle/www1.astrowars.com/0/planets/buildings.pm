769f0f18741c1d2e73b8d3e11f301280a408a461
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2007-05-22 18:14:31 +0000

    only add spend-all with PP>=150

--- mangle/www1.astrowars.com/0/planets/buildings.pm
+++ mangle/www1.astrowars.com/0/planets/buildings.pm
@@ -1,5 +1,7 @@
-s%</tr>\n</table>\n<br>%<td>|</td>
+if(m!<td>Sum</td><td>\d+</td><td>\d+</td><td>\d+</td><td>\d+</td><td>\d+</td><td>(\d+)</td></tr>! && $1>=150) {
+   s%</tr>\n</table>\n<br>%<td>|</td>
 <td><a href="/0/Planets/Spend_All_Points.php"><b>Spend All Points</b></a></td>$&%;
+}
 
 
 sub replace_buildings_colour
