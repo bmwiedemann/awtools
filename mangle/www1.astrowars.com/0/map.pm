85c88bbfd5a69d461c4a1115e222cc4ccfe7d52c
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2007-11-10 20:34:27 +0000

    fix tactical map link

--- mangle/www1.astrowars.com/0/map.pm
+++ mangle/www1.astrowars.com/0/map.pm
@@ -1,4 +1,4 @@
-s!<b>Map</b></td>!$&<td>$::bmwlink/tactical-large">AWtools(map)</a></td><td>|</td>!;
+s!<b>Map</b></td>!$&<td>$::bmwlink/tactical-live2">AWtools(map)</a></td><td>|</td>!;
 
 s%<tr><td><table cellpadding="0" cellspacing="0" border="0" background="/images/startile.gif" width="600"%$& class="map_inner"%;
 
