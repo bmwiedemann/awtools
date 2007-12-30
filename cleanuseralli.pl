7eb230884ba69f8f4e4707c81321ca1a4f81cdd4
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2007-12-30 19:03:27 +0000

    fix cleanup for DB usage

--- cleanuseralli.pl
+++ cleanuseralli.pl
@@ -8,23 +8,21 @@ use Fcntl qw(:flock O_RDWR O_CREAT O_RDONLY);
 awinput_init(1);
 
 if(!$interbeta) {
-   my %alliuser;
-   awinput::opendb(O_RDWR, "$awstandard::dbmdir/useralli.dbm", \%alliuser);
+   my $dbh=get_dbh;
+	my $userlist=$dbh->selectall_arrayref("SELECT `pid` FROM `useralli` ORDER BY `pid`");
    my @droplist=();
-   foreach my $k (keys %alliuser) {
-      my $pid=playername2id($k);
-      if(!$pid) {push(@droplist,$k);next} # wipe non-existent players
+   foreach my $e (@$userlist) {
+		my($kpid)=@$e;
+		my($pid,$k)=get_one_row("SELECT `pid`,`name` FROM `player` WHERE `pid` = ? LIMIT 1", [$kpid]);
+      if(!$pid) {push(@droplist,[$kpid,"*resigned*"]);next} # wipe non-existent players
       my $a=playerid2tag($pid);
       if(lc($k) eq "klappstuhl") {next}
-      if($a) {push(@droplist,$k);next} # drop tagged players
+      if($a) {push(@droplist,[$pid,$k]);next} # drop tagged players
    }
-   my $dbh=get_dbh;
    my $sth=$dbh->prepare("DELETE FROM `useralli` WHERE pid = ?");
-   foreach my $k (@droplist) {
-      print "dropped $k = $alliuser{$k}\n";
-      delete $alliuser{$k};
-      my $pid=playername2idm($k);
-      next if ! $pid;
+   foreach my $e (@droplist) {
+		my($pid,$k)=@$e;
+      print "dropped $pid=$k\n";
       $sth->execute($pid);
    }
 }
