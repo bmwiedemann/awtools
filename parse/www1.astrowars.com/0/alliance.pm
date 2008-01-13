a1f176780d94a3575662779ba24599386138be86
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-01-13 06:32:11 +0000

    add parsers

---
+++ parse/www1.astrowars.com/0/alliance.pm
@@ -0,0 +1,39 @@
+use strict;
+use awparser;
+
+my @members=();
+foreach my $line (m{<tr align=center bgcolor=(.+?)</tr>}gs) {
+	if($line=~m/^"?#303030/) {
+		my @a=split("</td><td>", $line);
+		while(@a) {
+			my($points,$x)=(shift(@a),shift(@a));
+			$points=~s/.*>(\d+)/$1/s;
+			if($x=~m{mode=post&u=(\d+)>([^<]+)</a>}) {
+				push(@members, [int($points), int($1), $2]);
+			}
+		}
+	} elsif($line=~m{^"#404040"><td>Name</td><td colspan="2">\s*<a href=(http://[^<]*)>([^<]*)</a></td>}) {
+		$d->{url}=$1;
+		$d->{name}=$2;
+	} elsif($line=~m{^"#404040"><td>Points</td><td colspan="2">(\d+)</td>}) {
+		$d->{points}=int($1);
+	} elsif($line=~m{^"#404040"><td colspan="3">\(average of the Top (\d+)\)</td>}) {
+		$d->{countingmembers}=int($1);
+	} elsif($line=~m{^"#404040"><td colspan=4>Members - (\d+)</td>}) {
+		$d->{members}=int($1);
+	} elsif($line=~m{^"#404040"><td>Leader</td><td colspan="2"><a href=/0/Player/Profile.php/\?id=(\d+)>([^<]+)</a></td>}) {
+		$d->{leader}=[int($1),$2];
+	} elsif($line=~m{^"#404040"><td>Tag</td><td colspan="2"><a href=/rankings/alliances/\w+.php>(\w+)</a></td>}) {
+		$d->{tag}=$1;
+	} elsif($line=~m{^"#894900.*all incomings}) {
+		$d->{premium}=1;
+	} else {
+		$d->{debug}=$line;
+	}
+}
+
+$d->{members}=\@members;
+
+# TODO: bool:isleader
+
+2;
---
+++ parse/www1.astrowars.com/0/alliance/info.pm
@@ -0,0 +1,26 @@
+use strict;
+use awparser;
+
+my @members=();
+foreach my $line (m{<tr align=center bgcolor=(.+?)</tr>}g) {
+	if(my($pid,$name,$rank,$points,$pl,$sl,$cl,$x,$y)=($line=~m{<td><a href=/0/Player/Profile.php/\?id=(\d+)>([^<]+)</a></td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(-?\d+)/(-?\d+)</td>})) {
+		my $counting=tobool($line=~m/^#303030/);
+		push(@members, {pid=>$pid, name=>$name, rank=>$rank, points=>$points,
+			pl=>$pl, cl=>$cl, x=>$x, y=>$y, counting=>$counting});
+	} elsif($line=~m{^#404040><td>#(\d+)\((\d+)\)</td><td>(\d+)</td><td><b>(\d+)</b></td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td>}) {
+		$d->{summary}={livingmembers=>$1, members=>$2, rank=>$3, points=>$4, pl=>$5, sl=>$6, cl=>$7};
+	} else {
+#		$d->{debug}=$line;
+	}
+}
+$d->{members}=\@members;
+
+if(m!<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#000000' width="600"><tr><td><center>([^<]{1,60}) - <small>(.*)!) {
+	$d->{name}=$1;
+	if($2=~m!(http://[^>]+)</a></small><br><table border=0!) {
+		$d->{url}=$1;
+	}
+	
+}
+
+2;
