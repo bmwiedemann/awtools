f916ee864ea87db893f57bccc065e5698e78e9ef
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-01-13 06:53:12 +0000

    add NAP parser

---
+++ parse/www1.astrowars.com/0/alliance/nap.pm
@@ -0,0 +1,17 @@
+use strict;
+use awparser;
+use awstandard;
+
+my @alliancenaps;
+my @playernaps;
+foreach my $line (m{<tr align=center bgcolor=(.+?)</tr>}g) {
+	if($line=~m{/0/Player/Profile.php/\?id=(\d+)>([^<]+)</a></td><td>(\d+:\d+:\d+ - [A-Z][a-z][a-z] \d\d)</td>$}) {
+		push(@playernaps, {pid=>int($1), name=>$2, established=>parseawdate($3)} );
+	} elsif($line=~m{/rankings/alliances/(\w+).php>([^<]+)</a></td><td>(\d+:\d+:\d+ - [A-Z][a-z][a-z] \d\d)</td>$}) {
+		push(@alliancenaps, {tag=>$1, name=>$2, established=>parseawdate($3)} );
+	}
+}
+$d->{alliancenaps}=\@alliancenaps;
+$d->{playernaps}=\@playernaps;
+
+2;
