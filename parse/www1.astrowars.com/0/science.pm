1659ba6d16112719adc68ebf29bba9d198746e79
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-01-15 18:13:32 +0000

    use more hashes

--- parse/www1.astrowars.com/0/science.pm
+++ parse/www1.astrowars.com/0/science.pm
@@ -2,33 +2,29 @@ use strict;
 use awparser;
 use awstandard;
 
-foreach my $what ("Culture", "Science") {
-   if(my @a=m!$what.*href="/0/Glossary//\?id=23">\(\+(\d+) per hour\)</a>((?:\s<b>[+-]\d+%)|)!) {
-      $a[1]=~s/.*([+-]\d+).*/$1/;
-      $a[1]||=0;
-      foreach my $a (@a) {$a+=0}
-      $d->{"\L$what"}=\@a;
-   }
+foreach my $line (m{<tr align=center bgcolor=(.+?)</tr>}gs) {
+
+	if($line=~m{^'#206060'><td><a href=/0/Glossary[^>]+>([^<]+)</a>}) {
+		$d->{currentscience}=$1;
+	}
+	if(my @a=($line=~m{([^>]+)</a> </td><td>(\d+)</td><td><img src="/images/dot.gif" height="10" width="(\d+)"><img src="/images/leer.gif" height="10" width="(\d+)"></td><td>(?:<form><input type="text" value=")?(\d+)(?:" size="8" name="r" class=text style="text-align:center;">)?</td><td>(?:<INPUT type="text" value=")?([0-9:]+(?: days)?)})) {
+      my $sl=$a[1]+($a[2]/250);
+		$d->{lc($a[0])}={level=>$sl, remain=>int($a[4]), "t"=>$a[5]};
+	}
+	elsif($line=~m{^"#202060"}) {
+		foreach my $what ("Culture", "Science") {
+			if(my @a=($line=~m!$what.*href="/0/Glossary//\?id=23">\(\+(\d+) per hour\)</a>((?:\s<b>[+-]\d+%)|)!)) {
+				$a[1]=~s/.*([+-]\d+).*/$1/;
+				$a[1]||=0;
+				foreach my $a (@a) {$a+=0}
+				$d->{lc($what)}={hourly=>$a[0], bonus=>$a[1]};
+			}
+		}
+	}
+	else {
+		$d->{debug}=$line;
+	}
 }
 
-
-my @science;
-foreach my $sci (@awstandard::sciencestr) {
-   if(m!$sci</a> </td><td>(\d+)</td><td><img src="/images/dot.gif" height="10" width="(\d+)"><img src="/images/leer.gif" height="10" width="(\d+)"></td><td>(?:<form><input type="text" value=")?(\d+)(?:" size="8" name="r" class=text style="text-align:center;">)?</td><td>(?:<INPUT type="text" value=")?([0-9:]+)!) {
-      my $sl=$1+($2/250);
-      push(@science, [$sci,$sl, int($4), $5]);
-		$d->{lc($sci)}=[$sl, int($4), $5];
-   }
-}
-$d->{sciencelevel}=\@science;
-
-if(m{<tr align=center bgcolor='#206060'><td><a href=/0/Glossary[^>]+>([^<]+)</a>}) {
-   $d->{currentscience}=$1;
-}
-
-if(m{id=14>Culture</a> </td><td>(\d+)</td><td><img src="/images/dot.gif" height="10" width="(\d+)"><img src="/images/leer.gif" height="10" width="\d+"></td><td>(?:<form><input type="text" value=")?(\d+)(?:" size="8" name="r" class=text style="text-align:center;">)?</td><td>(?:<INPUT type="text" value=")?([0-9:]+)}) {
-   my $cl=$1+$2/250;
-   $d->{culturelevel}=[$cl, int($3), $4];
-}
 
 1;
--- parse/www1.astrowars.com/0/trade.pm
+++ parse/www1.astrowars.com/0/trade.pm
@@ -2,7 +2,6 @@ use strict;
 use awparser;
 
 if($::options{url}=~m/Trade\/$/) {
-
    my @p=();
    my @arti=();
    foreach my $line (m{<tr bgcolor=(.+?)</tr>}g) {
@@ -11,12 +10,12 @@ if($::options{url}=~m/Trade\/$/) {
 #         $d->{$code}=[$code,$name,$value];
          push(@p, [$code,$name,unprettyprint($value)]);
       } elsif($line=~m{#(\d+)'><td>&nbsp;([a-zA-Z ]+ \d)</td><td align=center>(\d+)</td>}) {
-         my $current=0;
+         my $active=0;
          if($1 ne "404040") {
-            $d->{currentartifact}=$2;
-            $current=1;
+            $d->{activeartifact}=$2;
+            $active=1;
          }
-         push(@arti, [$2, int($3), $current]);
+         push(@arti, {name=>$2, quantity=>int($3), active=>$active});
       } elsif($line=~m{id=33>Supply Units</a></td><td align=center>(\d+)/(\d+)}) {
          $d->{su}=[int($1),int($2)];
       } elsif($line=~m{id=48>Astro Dollars</a></td><td align=center>\$([-+0-9.,]+)</b>}) {
