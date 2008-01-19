74b82c8f48c2c0b98e1e7de8eb1c0cc63ae23252
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-01-19 11:04:43 +0000

    add content types to parsed data

--- mangle/www1.astrowars.com/json.pm
+++ mangle/www1.astrowars.com/json.pm
@@ -2,6 +2,7 @@ use strict;
 use JSON::XS;
 use parse::dispatch;
 
+my $r=$::options{req};
 $::options{url}=~s{^http://www1\.astrowars\.com/json/}{http://www1.astrowars.com/};
 if($::options{url}=~s/.debug=1$//) { $::options{debug}=1; }
 if($::options{url}=~s/.callback=([a-zA-Z_]\w+)$//) { $::options{callback}=$1; }
@@ -14,6 +15,7 @@ if($::options{callback}) {
 	$postfix=" );";
 }
 if(!$::options{debug}) {
+	$r->content_type("application/x-javascript");
    $_=$prefix;
 	$_.=encode_json($data);
 	$_.=$postfix
--- mangle/www1.astrowars.com/xml.pm
+++ mangle/www1.astrowars.com/xml.pm
@@ -2,11 +2,13 @@ use strict;
 use XML::Simple;
 use parse::dispatch;
 
+my $r=$::options{req};
 $::options{url}=~s{^http://www1\.astrowars\.com/xml/}{http://www1.astrowars.com/};
 if($::options{url}=~s/.debug=1$//) { $::options{debug}=1; }
 my $data=parse::dispatch::dispatch(\%::options);
 
 if(!$::options{debug}) {
+	$r->content_type("text/xml");
    $_="";
 }
 $_.=XMLout($data);
