49b1d9cf1ec08394ed2d34a8fb26f75611d822ca
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-01-19 07:35:43 +0000

    add json callback

--- mangle/www1.astrowars.com/json.pm
+++ mangle/www1.astrowars.com/json.pm
@@ -4,11 +4,19 @@ use parse::dispatch;
 
 $::options{url}=~s{^http://www1\.astrowars\.com/json/}{http://www1.astrowars.com/};
 if($::options{url}=~s/.debug=1$//) { $::options{debug}=1; }
+if($::options{url}=~s/.callback=([a-zA-Z_]\w+)$//) { $::options{callback}=$1; }
 my $data=parse::dispatch::dispatch(\%::options);
 
+my $prefix="";
+my $postfix="";
+if($::options{callback}) {
+	$prefix=$::options{callback}."( ";
+	$postfix=" );";
+}
 if(!$::options{debug}) {
-   $_="";
+   $_=$prefix;
 	$_.=encode_json($data);
+	$_.=$postfix
 }
 else {
 	$_.=JSON::XS->new->utf8->pretty->encode($data);
--- preproc/www1.astrowars.com/json.pm
+++ preproc/www1.astrowars.com/json.pm
@@ -4,6 +4,7 @@ my $r=$::options{request};
 my $uri=$::options{url};
 #$uri=~s{^http://www1\.astrowars\.com/0/json/}{http://www1\.astrowars\.com/0/};
 $uri=~s{^http://www1\.astrowars\.com/json/}{http://www1\.astrowars\.com/};
+$uri=~s/.callback=([a-zA-Z_]\w+)$//;
 $r->uri($uri);
 
 2; # means go on with normal processing
