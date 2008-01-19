5aa6ebc4fe29a0210071967febdc16211b2fe20d
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-01-19 14:09:03 +0000

    hashify

--- parse/www1.astrowars.com/0/alliance/detail.pm
+++ parse/www1.astrowars.com/0/alliance/detail.pm
@@ -32,11 +32,15 @@ foreach my $line (m{<tr align=center bgcolor=(.+?)</tr>}g) {
 		if($d[3] eq "N/A") { push(@extra, foreignplanet=>1); splice(@d,0,5); }
 		foreach my $a (@ships) {$a+=0}
 		foreach my $a (@d) {$a+=0}
+		if(defined($d[0])) {
+			foreach my $n (0..4) { push(@extra, lc($awstandard::buildingstr[$n]), $d[$n])}
+		}
+		foreach my $n (0..4) { push(@extra, lc($awstandard::shipstr[$n]), $ships[$n])}
 		
-		push(@planets, {sid=>int($sid), pid=>int($pid), "pop"=>int($pop), name=>$a[1], siege=>$siege, buildings=>\@d, ships=>\@ships, @extra});
+		push(@planets, {sid=>int($sid), pid=>int($pid), "pop"=>int($pop), name=>$a[1], siege=>$siege, building=>\@d, ship=>\@ships, @extra});
 	} 
 }
-$d->{planetdata}=\@planets;
+$d->{planet}=\@planets;
 $d->{movingfleet}=\@movingfleet;
 
 # trades
@@ -44,10 +48,10 @@ if(m{<tr bgcolor=#303030><td colspan=2>(.+?)<br></td></tr>}) {
 	my @a=split("<br>",$1);
 	foreach my $a (@a) {
 		if($a=~m{\?id=(\d+)>([^<]+)</a>}) {
-			$a=[int($1),$2];
+			$a={pid=>int($1), name=>$2};
 		}
 	}
-	$d->{trades}=\@a;
+	$d->{trade}=\@a;
 }
 
 # race
@@ -58,27 +62,30 @@ if(m{<ul type=square><li>(.*?)</li></ul></td>}) {
 		if($a eq "Start Up Lab") { $d->{racesul}=1 }
 		if($a=~m{^([-+]\d+)% (\w+) \(([-+]\d+)\)$}) {
 			$a=[$2, int($1), int($3)];
-			$d->{"race$2"}=[int($1), int($3)];
+			$d->{"race$2"}={percent=>int($1), n=>int($3)};
 		}
 	}
-	$d->{race}=\@a;
+#	$d->{race}=\@a;
 }
 
 # sciences and other bottom info
 foreach my $line (m{<tr><td bgcolor=#303030>(.+?)</td></tr>}g) {
 	my @a=split(/<\/td><td[^>]*>/,$line);
+	my @extra=();
 	my $x=lc($a[0]);
 	if($a[0] eq "Trade Revenue") {chop($a[1])}
 	elsif($a[0] eq "Planets" && $a[1]=~m/^ (\d+) of (\d+)$/) {
-		(@a[1,2])=($1,$2);
+		$a[1]=$1;
 		$d->{culture}=$2;
 	}
-	if($x=~m/=/) { $x='pointcomposition'; $a[0]=~s/ =$//;$a[0]=[map{int($_)} split(' \+ ',$a[0])]; }
+	if($x=~m/=/) { $x='pointcomposition'; $a[0]=~s/ =$//;my @b=map{int($_)} split(' \+ ',$a[0]); 
+		@extra=("pop"=>$b[0], pl=>$b[1], sci=>$b[2]);
+	}
 	else { $x=~s/[^a-z0-9]//g; }
-	$a[1]=~s/^\+(\d+)\/h$/$1/;
+	$a[1]=~s/^\+(\d+)\/h$/$1/; # for hourly productions
 	$a[1]=toint($a[1]);
 	
-	$d->{$x}=\@a;
+	$d->{$x}={title=>$a[0], value=>$a[1], @extra};
 }
 
 if(m{<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#000000' width="600"><tr><td><center>([^<\n]*)<br>}) {
