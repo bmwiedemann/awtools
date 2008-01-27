673faf7199e827b6cc908f538131ba91a4187f32
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-01-27 19:15:21 +0000

    parse more

--- parse/www1.astrowars.com/0/planets.pm
+++ parse/www1.astrowars.com/0/planets.pm
@@ -10,7 +10,7 @@ if($::options{url}=~m{Planets/$}) {
 $d->{"spend_all_points"}=tobool(m{<td><a href="/0/Planets/Spend_All_Points.php"><b>Spend All Points</b></a></td>});
 
 my @p;
-foreach my $pline (m{<tr align=center bgcolor="#404040"(.+?)</tr>}g) {
+foreach my $pline (m{<tr align=center bgcolor=(.+?)</tr>}g) {
 	if(my @a=($pline=~m{\?i=(\d+)>([^<]+)</a></td><td>(\d+)</td><td> <img src="/images/dot.gif" height="10" width="(\d+)"><img src="/images/leer.gif" height="10" width="(\d+)"></td><td>\+(\d+)</td><td>(\d+)</td><td>\+(\d+)</td>})) {
 		for my $n(0,2..7) {$a[$n]+=0;}
       my @b=splice(@a,3,2);
@@ -20,14 +20,16 @@ foreach my $pline (m{<tr align=center bgcolor="#404040"(.+?)</tr>}g) {
 		for my $n(0..5) {$a{$label[$n]}=$a[$n]}
 		push(@p, \%a);
 	}
+	elsif (@a=($pline=~m{^'#303030'><td colspan=4>Growth ([+-]\d+)%.*Production ([+-]\d+)%</td><td>(-?\d+)</td><td>([+-]\d+)</td>$})) {
+		foreach my $a (@a) {$a+=0}
+		$d->{growthbonus}=shift(@a);
+		$d->{productionbonus}=shift(@a);
+		$d->{totalpp}=shift(@a);
+		$d->{hourlypp}=shift(@a);
+	}
 }
 $d->{planet}=\@p;
 
-if(my @a=m{<td colspan=4>Growth ([+-]\d+)%.*Production ([+-]\d+)%</td>}) {
-	foreach my $a (@a) {$a+=0}
-	$d->{growthbonus}=$a[0];
-	$d->{productionbonus}=$a[1];
-}
 }
 
 2;
