6c22470e3d28bed1e0ee6cd162d5f575a84b458f
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-02-11 20:08:47 +0000

    fix pp/ad price retrieval

--- mangle/www1.astrowars.com/0/planets.pm
+++ mangle/www1.astrowars.com/0/planets.pm
@@ -6,6 +6,6 @@ sub calcbonus($$) {
 
 s!(?:Growth [+-]\d+%&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)?Production ([+-]\d+)%</td><td>\d+</td><td>\+(\d+)!"$& = ".calcbonus($2,$1)!e;
 
-s!(Production [+-]\d+%</td><td>)(\d+)!$1.$2."=".int($2*$awinput::prices{pp}).'A$'!e;
+s!(Production [+-]\d+%</td><td>)(\d+)!$1.$2."=".int($2*awinput::getartifactprice("pp")).'A$'!e;
 
 1;
