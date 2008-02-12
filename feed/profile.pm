88fd77fe6a1b67cf9dc0d1dfaa6ed824a84c4580
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-02-12 20:14:53 +0000

    update using parsed data

--- feed/profile.pm
+++ feed/profile.pm
@@ -1,19 +1,24 @@
 use Tie::DBI;
+#use strict;
 use awstandard;
 use awinput;
 
 sub feed_profile() {
 print "profile feed\n";
+my $data=getparsed(\%::options);
 
-m/<html><head><title>(\S*)/;
-my $name=$1;
-my $opid=playername2id($name);
+my $name=$data->{name};
+my $opid=$data->{pid};
+#m/<html><head><title>(\S*)/;
+#my $name=$1;
+#my $opid=playername2id($name);
 print a({-href=>"relations?name=$name"},$name).br."\n";
+my $pl=$data->{playerlevel};
+my $pointsh=$data->{points};
 
-my ($pl)=(m!>Playerlevel</td><td>(\d+)!);
-if(m!>Points: (\d+)</td><td>(\d+)! && $opid) {
-   my $points=$1;
-   my $totalpop1=$2;
+if($pointsh && $opid) {
+   my $points=$pointsh->{total};
+   my $totalpop1=$pointsh->{"pop"};
 #   use DB_File;
 #   my %pointsdb;
 #   tie(%pointsdb, "DB_File", "/home/bernhard/db/points.dbm") or print "\nerror accessing DB\n";
@@ -25,22 +30,21 @@ if(m!>Points: (\d+)</td><td>(\d+)! && $opid) {
    my $now=time();
    my $time=$now-3600*9;
    $dbh->do("DELETE FROM cdcv WHERE pid = $opid"); # delete old entries
-   tie(%h,'Tie::DBI',$dbh,'cdcv','sidpid',{CLOBBER=>3});
+   tie(%h,'Tie::DBI',$dbh,'cdcv','sidpid',{CLOBBER=>1});
    my $totalpop=0;
-   foreach my $n (1..30) {
-      if(my($sid,$pid,$pop,$cv)=(m%<tr bgcolor="#303030" align=center><td>$n</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td>%)) {
-         my($sidpid)=sidpid22sidpid3m($sid,$pid);
+   foreach my $p (@{$data->{planet}}) {
+      my($pop,$cv)=($p->{"pop"}, $p->{cv});
+      my($sidpid)=sidpid22sidpid3m($p->{sid},$p->{pid});
 #         awdiag("test3 $n $sid#$pid=$sidpid $pop $cv");
-         $h{$sidpid}={time=>$now, cv=>$cv, pop=>$pop, pid=>$opid};
-         $totalpop+=$pop;
-      }
+      $h{$sidpid}={time=>$now, cv=>$cv, pop=>$pop, pid=>$opid};
+      $totalpop+=$pop;
    }
    untie(%h);
    if($totalpop1 != $totalpop) {
       print STDERR "profile feed pop sum mismatch: '$totalpop1'!='$totalpop'\n";
    } else {
       $dbh->do("DELETE FROM cdlive WHERE pid = $opid"); # delete old entries
-      tie(%h,'Tie::DBI',$dbh,'cdlive','pid',{CLOBBER=>3});
+      tie(%h,'Tie::DBI',$dbh,'cdlive','pid',{CLOBBER=>1});
       $h{$opid}={time=>$now, points=>$points, pl=>$pl, totalpop=>$totalpop};
       untie %h;
    }
@@ -50,8 +54,9 @@ if(m!>Points: (\d+)</td><td>(\d+)! && $opid) {
    my @a=(m%<a href=/about/playerprofile\.php\?id=(\d+)>[^<]+</a><br>%g);
    awinput::add_trades($opid,\@a);
 
-   if(m!>Trade Revenue</td><td bgcolor="?#202020"?>(\d+)%</td>!) {
-      $dbh->do("REPLACE INTO `tradelive` VALUES ($opid,$1)");
+   if((my $tr=$data->{traderevenue})) {
+      $tr+=0;
+      $dbh->do("REPLACE INTO `tradelive` VALUES ($opid,$tr)");
    }
 }
 
