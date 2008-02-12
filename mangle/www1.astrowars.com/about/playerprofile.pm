a07a931d5ac93dd9835595e6155e018a9376a9eb
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-02-12 20:13:59 +0000

    cleanup after ARs improvement

--- mangle/www1.astrowars.com/about/playerprofile.pm
+++ mangle/www1.astrowars.com/about/playerprofile.pm
@@ -1,6 +1,5 @@
+use awinput;
 use awsql;
-use parse::dispatch;
-my $data=parse::dispatch::dispatch(\%::options);
 
 if($::options{url}=~m%^http://www1\.astrowars\.com/about/playerprofile.php\?((?:id)|(?:name))=(.+)%) { 
    my $id=$2;
@@ -11,16 +10,5 @@ if($::options{url}=~m%^http://www1\.astrowars\.com/about/playerprofile.php\?((?:
    if($arg eq "name") {$pid=playername2idm($id);}
    update_premium($pid, $prem);
 }
-
-my $totalpop10=0;
-foreach my $planet (@{$data->{planet}}) {
-	my $pop10=$planet->{"pop"}-10;
-	next if $pop10<=0;
-	$totalpop10+=$pop10;
-}
-my $plpoints=$data->{playerlevel};
-my $scipoints=$data->{points}->{total} - $data->{points}->{"pop"} - $plpoints*2;
-my $totalpoints=$totalpop10+$plpoints+$scipoints;
-s{colspan=3>Points: \d+</td><td>\d+</td><td>\d+</td></tr>}{$&<tr bgcolor="#306030"><td colspan="3" align="center">points = $totalpop10+$plpoints+$scipoints</td><td>$totalpoints</td></tr>};
 
 1;
