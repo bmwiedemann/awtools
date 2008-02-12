ecae176b053684a3e56679ec690318005b26b21a
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-02-12 20:15:09 +0000

    minor improvement

--- mangle/www1.astrowars.com/rankings/alliances.pm
+++ mangle/www1.astrowars.com/rankings/alliances.pm
@@ -1,6 +1,6 @@
 if($::options{url}=~m%^http://www1\.astrowars\.com/rankings/alliances/(\w+)\.php%) { 
    my $tag=$1;
-   s%^</td></tr></table>%$& $::bmwlink/alliance?alliance=$tag">AWtools($tag)</a><br>%m;
+   s%^</td></tr></table>%$& $::bmwlink/alliance?alliance=$tag">AWtools($tag)</a> <a href="/0/Alliance/Info.php?tag=$tag">AW $tag</a><br>%m;
 }
 
 2;
