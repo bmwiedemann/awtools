ba861fadcbdf27a526e6f3904c3d3b56881547b6
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-03-22 16:35:56 +0000

    fix free planets shown as sieged

--- parse/www1.astrowars.com/0/map/detail.pm
+++ parse/www1.astrowars.com/0/map/detail.pm
@@ -9,7 +9,7 @@ $d->{y}=int($sysy);
 my @planet=();
 foreach my $line (m{<tr bgcolor=("#\d+" align=center><td>.+?)</tr>}g) {
    if($line=~m{#(\d+)" align=center><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(?:<a href=/0/Player/Profile.php/\?id=(\d+)>)?([^<]+)}) {
-      push(@planet, {id=>int($2), sieged=>tobool($1 ne "404040"), "population"=>int($3), starbase=>int($4), pid=>int($5), name=>$6});
+      push(@planet, {id=>int($2), sieged=>tobool($1 eq "602020"), "population"=>int($3), starbase=>int($4), pid=>int($5), name=>$6});
    }
 }
 $d->{planet}=\@planet;
