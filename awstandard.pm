5cce8273cce3351fd572527d07db9f8c8a3eef18
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-04-20 18:53:07 +0000

    add variable awwwwserver

--- awstandard.pm
+++ awstandard.pm
@@ -9,7 +9,7 @@ our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
 $VERSION = sprintf "%d.%03d", q$Revision$ =~ /(\d+)/g;
 @ISA = qw(Exporter);
 @EXPORT = 
-qw(&awstandard_init &bmwround &bmwmod &awdiag &AWheader3 &AWheader2 &AWheader &AWtail &AWfocus &wikilink
+qw(&awstandard_init &bmwround &bmwmod &awdiag &AWheader3 &AWheader2 &AWheader &AWtail &AWfocus &wikilink getawwwwserver
 &mon2id &parseawdate &getrelationclass &getrelationcolor &getstatuscolor &planetlink &profilelink &alliancedetailslink &systemlink &alliancelink &addplayerir &fleet2cv &addfleet &relation2race &relation2science &gmdate &AWtime &AWisodate &AWisodatetime &AWreltime &sb2cv &title2pm &safe_encode &html_encode &file_content &url2pm &awmax &awmin &getauthpid &getparsed
       $magicstring $style $server $awserver $bmwserver $toolscgiurl $timezone %planetstatusstring %relationname $interbeta $basedir $dbdir @racebonus %artifact);
 
@@ -92,6 +92,13 @@ sub awstandard_init() {
 }
 # free locks & other critical resources
 sub awstandard_finish() {
+}
+
+sub getawwwwserver() {
+	if($awserver=~m/^aw21/) {
+		return "www.$awserver";
+	}
+	return $awforumserver;
 }
 
 sub bmwround($) { my($number)=@_;
