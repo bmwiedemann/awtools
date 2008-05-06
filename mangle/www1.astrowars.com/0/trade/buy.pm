74f755e698eb537dbf442e94adba6f8c7aa804c2
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2008-05-06 04:02:36 +0000

    +usability JS

--- mangle/www1.astrowars.com/0/trade/buy.pm
+++ mangle/www1.astrowars.com/0/trade/buy.pm
@@ -6,4 +6,9 @@ s%(<b>Agreement</b></a></td>)(\n<td>|</td><td><a href="?/0/Trade/Buy)%$1
 <td>|</td><td><a href="/0/Trade/Artifacts.php"><b>Artifacts</b></a></td>
 <td>|</td><td><a href="/0/Trade/Sell.php"><b>Sell</b></a></td>$2%;
 
+
+# add javascript to enable PP radio on number change
+s/method="post"/$& name="form"/;
+s/<input type="text" name="proqty"/$& onblur=document.form.buy[0].checked=true;/;
+
 1;
