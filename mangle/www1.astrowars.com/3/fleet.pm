394a128b501935ee4840b242243da114241d25bd
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2009-02-16 05:24:22 +0000

    set title

--- mangle/www1.astrowars.com/3/fleet.pm
+++ mangle/www1.astrowars.com/3/fleet.pm
@@ -10,7 +10,7 @@ $_='<?xml version="1.0" encoding="iso-8859-1"?>
 <!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US"><head><title>index</title>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US"><head><title>Fleet</title>
 <link rel="stylesheet" type="text/css" href="http://aw.lsmod.de/code/css/awmod.css" />
 <link type="image/vnd.microsoft.icon" rel="icon" href="http://aw.lsmod.de/awfavicon.ico" />
 <link rel="shortcut icon" href="http://aw.lsmod.de/awfavicon.ico" />
