0a15e3b56ddeee5bf0eab7d12d10a8e9eda7dfe2
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2009-03-04 13:24:07 +0000

    add extra info column to ranking page

--- mangle/www1.astrowars.com/rankings/bestplanets.pm
+++ mangle/www1.astrowars.com/rankings/bestplanets.pm
@@ -1,4 +1,8 @@
 s%(<tr bgcolor="#262626" align=center><td>\d+</td><td>)(<a href=/about/starmap.php\?dx=)([+-]?\d+)&dy=([+-]?\d+)>([^<]*</a></td>)%$1."<a href=\"/0/Map/Detail.php/?nr=".systemcoord2id($3,$4)."\">s</a> ".$2.$3."&dy=".$4.">".$5%ge;
 
+# add Total column:
+s{<td>Owner</td>}{<td>Total</td>$&};
+s{<tr bgcolor="#262626" align=center><td>\d+</td><td>.*?</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td>}{"$&<td>".($1+$2+$3+$4)."</td>"}ge;
+
 
 2; # mangle on upwards
