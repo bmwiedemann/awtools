d21e7545c26579b13f3a0ca0367110634d0f7688
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2009-03-08 11:01:19 +0000

    fix tools links with spaces

--- mangle/www1.astrowars.com/about/starmap.pm
+++ mangle/www1.astrowars.com/about/starmap.pm
@@ -1,3 +1,9 @@
-s%<b>([^<]+)(</b>\n<small>)%<b>$::bmwlink/system-info?name=$1">$1</a>$2%;
+sub escape($)
+{ my($x)=@_;
+	$x=~s/ /+/;
+	return $x;
+}
+
+s%<b>([^<]+)(</b>\n<small>)%"<b>$::bmwlink/system-info?name=".escape($1).qq'">$1</a>$2'%e;
 
 1;
