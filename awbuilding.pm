cacc99224f1c85145da8e3d7261e371c3d0590d2
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2009-04-18 13:12:05 +0000

    feed extra planet data

---
+++ awbuilding.pm
@@ -0,0 +1,55 @@
+#
+# manage building DB feeding for AW
+#
+package awbuilding;
+use strict;
+use warnings;
+use DBAccess2;
+use awinput;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = 
+qw(&update_building);
+our $f=1000;
+
+sub update_building($$$%)
+{
+   my($sid, $pid,$isfloat,$p)=@_;
+   return if not $sid || not $pid;
+	my $sidpid=sidpid22sidpid3m($sid,$pid);
+	my $alli=$ENV{REMOTE_USER};
+	if(!$alli || $alli eq "guest") {return}
+   my $dbh=get_dbh;
+	my $sth=$dbh->prepare_cached("SELECT * FROM `internalplanet` WHERE alli=? AND sidpid=?");
+	my @oldv=$dbh->selectrow_array($sth, {}, $alli,$sidpid);
+	my @v=($p->{pop}, $p->{pp}, $p->{hf}, $p->{rf}, $p->{gc}, $p->{rl}, $p->{sb});
+	foreach my $v (@v) {
+		$v*=$f;
+	}
+	print "feed old:@oldv new:@v<br/>";
+	my @newv=@oldv;
+	if($isfloat || !defined($oldv[0])) {
+		@newv=@v;
+	} else {
+		for my $i (0..6) {
+			my $v=$v[$i]; 
+			if($oldv[$i]<$v || $v<$f*int($oldv[$i]/$f)) {
+				# force changes even with rounded input
+				$newv[$i]=$v;
+			}
+		}
+	}
+
+	if(!defined($oldv[0])) {
+		my $sth2=$dbh->prepare_cached("INSERT INTO `internalplanet` VALUES (?,?,?,?,?,?,?,?,?)");
+   	$sth2->execute($alli,$sidpid, @v) or 
+		print "err: ",$sth2->errstr,"<br/>";
+	} else {
+		my $sth2=$dbh->prepare_cached("UPDATE `internalplanet` SET pop=?, pp=?, b1=?, b2=?, b3=?, b4=?, b5=? WHERE alli=? AND sidpid=?");
+   	$sth2->execute(@v, $alli,$sidpid) or 
+		print "err: ",$sth2->errstr,"<br/>";
+	}
+}
+
+1;
--- feed/www1.astrowars.com/0/planets/detail.pm
+++ feed/www1.astrowars.com/0/planets/detail.pm
@@ -1,13 +1,24 @@
 use awstandard;
+use awbuilding;
+
+my $d=getparsed(\%::options);
 
 #awdiag("planets feeding ".length($_));
 #open(F, ">/tmp/)
-if(m!Transport</a></td><td>(\d+).* Colony Ship</a></td><td>(\d+).* Destroyer</a></td><td>(\d+).* Cruiser</a></td><td>(\d+).* Battleship</a></td><td>(\d+).*<a href="?/0/Player/Profile.php/\?id=(\d+)"?>Hostile forces in the orbit of ([^\!]*) (\d+)\!!s) {
+if($d->{sieging} && m!Transport</a></td><td>(\d+).* Colony Ship</a></td><td>(\d+).* Destroyer</a></td><td>(\d+).* Cruiser</a></td><td>(\d+).* Battleship</a></td><td>(\d+).*<a href="?/0/Player/Profile.php/\?id=(\d+)"?>Hostile forces in the orbit of ([^\!]*) (\d+)\!!s) {
+	# TODO optimize my @fleet=($d->{colonyship}->{num},...,$d->{battleship}->{num}||0);
    my @fleet=($1,$2,$3,$4,$5);
    my ($owner, $sid, $pid) = ($6, systemname2id($7), $8);
 #   awdiag("@fleet $6 sieges $sid $pid");
    dbplanetaddinit(undef, 8);
    dbfleetadd($sid, $pid, $owner, "", 0, 1, \@fleet);
 };
+
+my %h=(pp=>$d->{productionpoints}->{num}, "pop"=>$d->{population}->{num},
+	hf=>$d->{hydroponicfarm}->{num}, rf=>$d->{roboticfactory}->{num}, 
+	gc=>$d->{galacticcybernet}->{num}, rl=>$d->{researchlab}->{num}, 
+	sb=>$d->{starbase}->{num} );
+
+update_building($d->{sid},$d->{pid},1,\%h);
 
 1;
