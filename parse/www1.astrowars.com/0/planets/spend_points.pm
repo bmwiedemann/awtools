c10ae8f08bf4a81feb94f388fe09ff02c96bc3b2
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2009-08-03 20:23:51 +0000

    add num of SUs

--- parse/www1.astrowars.com/0/planets/spend_points.pm
+++ parse/www1.astrowars.com/0/planets/spend_points.pm
@@ -2,9 +2,12 @@ use strict;
 use awparser;
 my @buildings=("Hydroponic Farm", "Robotic Factory", "Galactic Cybernet", "Research Lab");
 
-for my $v (@buildings) {
-	my $key=lc($v); $key=~s/ //;
-   ($d->{"su$key"})=(m/$v <br>get \$(\d+)<\/td>/);
+if(m{Where to spend a <a href=/0/Glossary//\?id=33>Supply Unit</a>\? \((\d+)\)}) {
+	$d->{su}=$1;
+	for my $v (@buildings) {
+		my $key=lc($v); $key=~s/ //;
+		($d->{"su$key"})=(m/$v <br>get \$(\d+)<\/td>/);
+	}
 }
 
 2;
