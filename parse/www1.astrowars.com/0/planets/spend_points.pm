0539a0477c4c5e92816d32f73130bbb1367e0204
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2009-08-03 20:31:20 +0000

    use integers for nicer JSON

--- parse/www1.astrowars.com/0/planets/spend_points.pm
+++ parse/www1.astrowars.com/0/planets/spend_points.pm
@@ -3,10 +3,11 @@ use awparser;
 my @buildings=("Hydroponic Farm", "Robotic Factory", "Galactic Cybernet", "Research Lab");
 
 if(m{Where to spend a <a href=/0/Glossary//\?id=33>Supply Unit</a>\? \((\d+)\)}) {
-	$d->{su}=$1;
+	$d->{su}=int($1);
 	for my $v (@buildings) {
 		my $key=lc($v); $key=~s/ //;
-		($d->{"su$key"})=(m/$v <br>get \$(\d+)<\/td>/);
+		(my $a)=(m/$v <br>get \$(\d+)<\/td>/);
+		$d->{"su$key"}=int($a);
 	}
 }
 
