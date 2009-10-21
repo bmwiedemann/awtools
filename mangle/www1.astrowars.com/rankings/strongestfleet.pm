36d6f8a5b8cf402e3298d1164a03ee188e3074b0
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2009-10-21 09:34:19 +0000

    add wiki link

--- mangle/www1.astrowars.com/rankings/strongestfleet.pm
+++ mangle/www1.astrowars.com/rankings/strongestfleet.pm
@@ -1,6 +1,6 @@
 
 # add CV column:
-s{<td>Owner</td>}{<td>CV</td>$&};
+s{<td>Owner</td>}{<td><a href="http://www.astrowars.com/portal/CV">CV</a></td>$&};
 s{<tr bgcolor="#262626" align=center><td>\d+</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td>}{"$&<td>".(($1+$2*8+$3*20)*3)."</td>"}ge;
 
 2;
