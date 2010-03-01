19dc460426b303ae7138ec956ae76bd7bdda530f
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2010-03-01 13:21:23 +0000

    add internal planet data predictions

--- awbuilding.pm
+++ awbuilding.pm
@@ -5,12 +5,15 @@ package awbuilding;
 use strict;
 use warnings;
 use DBAccess2;
+use awsql;
 use awinput;
 
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = 
-qw(&getbuilding &update_building &update_building_pp);
+qw(&getbuilding &getbuilding_sidpid &getbuilding_player &update_building &update_building_pp);
+
+my $expiry=15*24*60*60;
 
 sub getbuilding($@)
 {
@@ -18,6 +21,20 @@ sub getbuilding($@)
 	my $dbh=get_dbh;
 	my $sth=$dbh->prepare_cached("SELECT * FROM `internalplanet` $cond");
 	return $dbh->selectall_arrayref($sth, {}, @$vars);
+}
+
+sub getbuilding_sidpid($$)
+{
+	my($sid,$pid)=@_;
+	my ($allimatch, $amvars)=get_alli_match2($ENV{REMOTE_USER},32);
+	return getbuilding(",toolsaccess WHERE $allimatch AND sidpid=? AND updated_at>?", [@$amvars, sidpid22sidpid3m($sid,$pid), time()-$expiry]);
+}
+
+sub getbuilding_player($)
+{
+	my($pid)=@_;
+	my ($allimatch, $amvars)=get_alli_match2($ENV{REMOTE_USER},32);
+	return getbuilding(",toolsaccess WHERE $allimatch AND ownerid=? AND updated_at>?", [@$amvars, $pid, time()-$expiry]);
 }
 
 sub update_building($$$%)
@@ -30,22 +47,26 @@ sub update_building($$$%)
    my $dbh=get_dbh;
 	my $sth=$dbh->prepare_cached("SELECT * FROM `internalplanet` WHERE sidpid=?");
 	my @oldv=$dbh->selectrow_array($sth, {}, $sidpid);
-	my @v=($p->{pop}, $p->{pp}, $p->{hf}, $p->{rf}, $p->{gc}, $p->{rl}, $p->{sb});
+	my @v=($p->{pop}, $p->{pp}, $p->{hf}, $p->{rf}, $p->{gc}, $p->{rl}, $p->{sb}, time());
 	#print "feed old:@oldv new:@v<br/>";
 	my $offs=4;
-	my @newv=@oldv;
+	my @newv;
 	if($isfloat || !defined($oldv[0])) {
 		@newv=@v;
 	} else {
-		for my $i (0..6) {
+		for my $i (0..7) {
 			my $v=$v[$i]; 
 			my $o=$oldv[$i+$offs];
-			if(defined($v) && ($o<$v || $v<int($o))) {
+			# only update pop from inexact values when integer part changed:
+			if(defined($v) && int($v)!=int($o)) {
+			#if(defined($v) && (abs($o-$v)>0.1 || $v<int($o))) {
 				# force changes even with rounded input
 				$newv[$i]=$v;
-			}
+			} else {$newv[$i]=$o}
 		}
 	}
+	for my $i (0..7) { $newv[$i]||=0; }
+	@v=@newv;
 	# TODO optimize: could return here if @newv==@oldv
 
 	unshift(@v, $p->{ownerid});
@@ -58,22 +79,34 @@ sub update_building($$$%)
 	unshift(@v, $u||time());
 
 	if(!defined($oldv[0])) {
-		my $sth2=$dbh->prepare_cached("INSERT INTO `internalplanet` VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+		my $sth2=$dbh->prepare_cached("INSERT INTO `internalplanet` VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
    	$sth2->execute($sidpid, @v) or 
 		print "err: ",$sth2->errstr,"<br/>";
 	} else {
-		my $sth2=$dbh->prepare_cached("UPDATE `internalplanet` SET time=?, alli=?, ownerid=?, pop=?, pp=?, b1=?, b2=?, b3=?, b4=?, b5=? WHERE sidpid=?");
-   	$sth2->execute(@v, $sidpid) or 
-		print "err: ",$sth2->errstr,"<br/>";
+		# diff
+		my $same=1;
+		for my $i (0..$#v) {
+			if($v[$i] != $oldv[$i]) {$same=0; last};
+		}
+		if(!$same) {
+			my $sth2=$dbh->prepare_cached("UPDATE `internalplanet` SET time=?, alli=?, ownerid=?, pop=?, pp=?, b1=?, b2=?, b3=?, b4=?, b5=?, updated_at=? WHERE sidpid=?");
+	   	$sth2->execute(@v, $sidpid) or 
+			print "err: ",$sth2->errstr,"<br/>";
+		} else {
+#			print "still same values - not updating<br/>\n";
+		}
 	}
 }
 
+# allow to auto-update pop+PP
 sub update_building_pp($%)
 {
 	my($sidpid,$h)=@_;
    my $dbh=get_dbh;
-	my $sth2=$dbh->prepare_cached("UPDATE `internalplanet` SET time=?, pp=? WHERE sidpid=?");
-	$sth2->execute($h->{"time"}, $h->{pp}, $sidpid);
+	my $sth2=$dbh->prepare_cached("UPDATE `internalplanet` SET time=?, pp=?, pop=? WHERE sidpid=?");
+	$sth2->execute($h->{"time"}, $h->{pp}, $h->{"pop"}, $sidpid);
 }
+
+#sub update_building_pop ?
 
 1;
