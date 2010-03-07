364424eab3f93a6d1ca7c2e992a0b460927b1c61
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2010-03-07 13:50:29 +0000

    parse sieged planets

--- parse/www1.astrowars.com/0/planets.pm
+++ parse/www1.astrowars.com/0/planets.pm
@@ -11,12 +11,12 @@ $d->{"spend_all_points"}=tobool(m{<td><a href="/0/Planets/Spend_All_Points.php">
 
 my @p;
 foreach my $pline (m{<tr align=center bgcolor=(.+?)</tr>}g) {
-	if(my @a=($pline=~m{\?i=(\d+)>([^<]+)</a></td><td>(\d+)</td><td> <img src="/images/dot.gif" height="10" width="(\d+)"><img src="/images/leer.gif" height="10" width="(\d+)"></td><td>\+(\d+)</td><td>(\d+)</td><td>\+(\d+)</td>})) {
+	if(my @a=($pline=~m{^"#(\d+).*\?i=(\d+)>([^<]+)</a></td><td>(\d+)</td><td> <img src="/images/dot.gif" height="10" width="(\d+)"><img src="/images/leer.gif" height="10" width="(\d+)"></td><td>\+(\d+)</td><td>(\d+)</td><td>\+(\d+)</td>})) {
+		my %a=(siege=>((shift(@a) eq "602020")?1:0));
 		for my $n(0,2..7) {$a[$n]+=0;}
       my @b=splice(@a,3,2);
       $a[2]+=$b[0]/100; # or /($b[0]+$b[1])
 		my @label=qw(id name population growth pp production);
-		my %a=();
 		for my $n(0..5) {$a{$label[$n]}=$a[$n]}
 		#require awinput;
 		#awinput::awinput_init(1);
--- parse/www1.astrowars.com/0/planets/buildings.pm
+++ parse/www1.astrowars.com/0/planets/buildings.pm
@@ -2,11 +2,11 @@ use strict;
 use awparser;
 
 my @p;
-foreach my $pline (m{<tr align=center bgcolor="#404040"(.+?)</tr>}g) {
-   if(my @a=($pline=~m!\?i=(\d+)>([^<]+)</a></td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td>!)) {
+foreach my $pline (m{<tr align=center bgcolor="#(\d+".+?)</tr>}g) {
+   if(my @a=($pline=~m!^(\d+).*\?i=(\d+)>([^<]+)</a></td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td>!)) {
+		my %a=(siege=>((shift(@a) eq "602020")?1:0));
       for my $n(0,2..7) {$a[$n]+=0;}
 		my @label=qw(id name population farm factory cybernet lab pp);
-		my %a=();
 		for my $n(0..7) {$a{$label[$n]}=$a[$n]}
       push(@p, \%a);
    }
