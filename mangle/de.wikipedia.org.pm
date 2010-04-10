abe616501033889a123cf5e4c5e60e2003098b39
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2010-04-10 08:29:42 +0000

    dont allow anon edit through proxy

--- mangle/de.wikipedia.org.pm
+++ mangle/de.wikipedia.org.pm
@@ -8,6 +8,9 @@ use Encode;
 #$_.="$ENV{REQUEST_URI} ; $ENV{QUERY_STRING}";
 $_=decode_utf8($_);
 if($ENV{REQUEST_URI}=~m{wikipedia.org/w/index.php.*action=(edit|submit)}) {
+	if(!m{<li id="pt-preferences"><a href="/wiki/Spezial:Einstellungen" title="Eigene Einstellungen">Einstellungen}) {
+		$_="denied. please log in first.";
+	}
    if(m{<textarea name="wpTextbox1" id="wpTextbox1" cols="80" rows="25" tabindex="1" accesskey="," style="width: 100%">([^<>]+)^</textarea>}m) {
 		my $text=$1;
 		typocheck::init();
@@ -20,7 +23,7 @@ if($ENV{REQUEST_URI}=~m{wikipedia.org/w/index.php.*action=(edit|submit)}) {
 
 if($ENV{REQUEST_URI}=~m{wikipedia.org/(wiki/Spezial:Search)|(w/index.php\?title=Spezial%3ASuche)}) {
 	s{<a href="/wiki/([^"]+)"}{<a href="/w/index.php?title=$1&action=edit">Edit</a> $&}g;
-#	$_.="OK";
 }
+#$_.="testOK";
 
 1;
