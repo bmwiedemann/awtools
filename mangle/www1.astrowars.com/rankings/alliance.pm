6444d6fe255b9ea94d4c28ecb0599b4f8201b61c
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2010-05-28 15:19:30 +0000

    link AW alli page

--- mangle/www1.astrowars.com/rankings/alliance.pm
+++ mangle/www1.astrowars.com/rankings/alliance.pm
@@ -1,4 +1,7 @@
 # add Rasta link
 s{Alliance Highscore</b>}{$& <a href="http://www.astrowars-tools.com/rankings.php?ranking=alliances" class="awtools">Rasta</a>};
 
+# link AW alli page
+s{(align=center><td>\d+</td><td>)([a-zA-Z]+)}{$1<a href="/0/Alliance/Info.php?tag=$2">$2</a>}g;
+
 2;
