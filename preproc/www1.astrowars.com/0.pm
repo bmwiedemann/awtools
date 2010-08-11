7541b71335490b791f13a64f14e8c040f66f1aaa
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2010-08-11 08:33:42 +0000

    prevent click ban on brownie side

--- preproc/www1.astrowars.com/0.pm
+++ preproc/www1.astrowars.com/0.pm
@@ -1,10 +1,18 @@
 use strict;
 
+#if($::options{name} ne "greenbird" || $::options{nclick}<300) {
 if($::options{nclick}<300) {
 	2; # all OK - go on processing normally
 } else {
 	$::options{mime}="text/html";
-	$_=qq(<html><body>This is a message from brownie: 
+	$_=qq(<html><head>
+ <title>brownie had too many clicks</title>
+<link rel="stylesheet" type="text/css" href="http://aw.lsmod.de/code/css/main.css">
+<link type="image/vnd.microsoft.icon" rel="icon" href="http://aw.lsmod.de/awfavicon.ico">
+<link rel="shortcut icon" href="http://aw.lsmod.de/awfavicon.ico">
+</head>
+<body><h1>brownie had too many clicks</h1>
+This is a message from brownie: 
 You have reached $::options{nclick} clicks and to prevent you from being blocked for flooding, brownie is not passing further requests to the game.<br>
 What you can do:
 <ol>
@@ -13,7 +21,7 @@ What you can do:
 <li>close all browser windows
 </ol>
 <hr>
-Please <a href="http://home.rebelstudentalliance.co.uk/forum/index.php/board,17.0.html">give feedback about this feature</a>
+Please <a href="http://forum.rebelstudentalliance.co.uk/index.php/board,17.0.html" class="awtools">give feedback about this feature</a>
 </body></html>
 );
 	1;
