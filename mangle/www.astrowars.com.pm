f6e967b030ec3ab4cc1f0ba5f842e1815b002aa9
Bernhard M. Wiedemann <bernhard+awtools lsmod de>
2010-08-28 11:28:42 +0000

    general checkin

---
+++ mangle/www.astrowars.com.pm
@@ -0,0 +1,3 @@
+s{(Free Online Multiplayer Game)(</title>)}{$1 - brownie version$2};
+
+1;
---
+++ startup.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use lib qw(/home/aw/inc /home/aw/base); # still needed
+chdir "/home/aw/inc";      # still needed
+use Apache::DBI;          # for speedup of mysql access with persistent conn
+use Apache2::Access;       # for $r->get_basic_auth_pw
+use Apache2::RequestIO (); # for $r->print
+use Apache2::RequestUtil (); # speedup
+use APR::Table;           # for $headers->unset
+use CGI;
+use Tie::DBI;
+use LWP::UserAgent ();
+use LWP::ConnCache;
+
+use apacheconst;
+use DBConf;
+use awinput;
+use awimessage;
+#use Apache2;
+#Apache::DBI->connect_on_init($DBConf::connectionInfo,$DBConf::dbuser,$DBConf::dbpasswd);
+Apache::DBI->setPingTimeOut($DBConf::connectionInfo, -1);
+
+if(0) {
+open(DEBUG, ">", "/tmp/debug123");
+print DEBUG scalar localtime(),"\n";
+close DEBUG;
+}
+
+1;
