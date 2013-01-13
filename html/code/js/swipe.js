025dd21099e31906cd8a34cc3a5f73770ec8fd3f
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2013-01-13 07:21:18 +0100

    adjust swiping to cover only the top_navi area

--- html/code/js/swipe.js
+++ html/code/js/swipe.js
@@ -3,7 +3,7 @@ var finalCoord={x:0,y:0}
 function touchStart(event){originalCoord.x=event.targetTouches[0].pageX
   originalCoord.y=event.targetTouches[0].pageY}
 function touchMove(event){finalCoord.x=event.targetTouches[0].pageX
-  finalCoord.y=event.targetTouches[0].pageY;if(Math.abs(finalCoord.y-originalCoord.y)<10)event.preventDefault();}
+  finalCoord.y=event.targetTouches[0].pageY;if(Math.abs(finalCoord.y-originalCoord.y)<touchdefaults.threshold.y)event.preventDefault();}
 function touchEnd(event){var changeY=originalCoord.y- finalCoord.y
   if(changeY<touchdefaults.threshold.y&&changeY>(touchdefaults.threshold.y*-1)){changeX=originalCoord.x- finalCoord.x
 	  if(changeX>touchdefaults.threshold.x){touchdefaults.swipeLeft()}
@@ -14,7 +14,7 @@ function touchStart(event){originalCoord.x=event.targetTouches[0].pageX
   finalCoord.y=originalCoord.y}
 
 function startup() {
-	var el = document.getElementsByTagName("body")[0];
+	var el = document.getElementsByClassName("top_navi")[0];
 	el.addEventListener("touchstart", touchStart, false);
 	el.addEventListener("touchend", touchEnd, false);
 	el.addEventListener("touchleave", touchEnd, false);
