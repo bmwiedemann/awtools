a821077296587b750d4b3ea7d22dc00bf317480e
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2013-08-21 07:59:42 +0200

    update to current AW

--- mangle/www1.astrowars.com/rankings.pm
+++ mangle/www1.astrowars.com/rankings.pm
@@ -2,6 +2,7 @@ use awinput;
 use awhtmlout;
 
 s%(<a href=/about/playerprofile\.php\?id=)(\d+)>([^<]*)</a>%"<a href=\"/0/Player/Profile.php/?id=$2\">p</a> ".$1.$2." class=\"".mangleplayerlink($2,$3)."</a>"%ge;
+s%(<a href="/about/playerprofile\.php\?id=)(\d+)">([^<]*)</a>%"<a href=\"/0/Player/Profile.php/?id=$2\">p</a> ".$1.$2."\" class=\"".mangleplayerlink($2,$3)."</a>"%ge;
 
 s%</head>%<link rel="stylesheet" type="text/css" href="http://aw.zq1.de/code/css/main.css">$&%;
 if($::options{url}=~m%^http://www1\.astrowars\.com/rankings/$%) {
