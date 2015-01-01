df2f63474dce914b909d79bc62c71e5b08192ac7
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2015-01-01 06:21:19 +0100

    use zq1.de domain

--- mangle/www.astrowars.com.pm
+++ mangle/www.astrowars.com.pm
@@ -1,3 +1,4 @@
 s{(Free Online Multiplayer Game)(</title>)}{$1 - brownie version$2};
+$::options{url}=~m{http://[^/]*/(.*)} && s{</head>}{<link rel="canonical" href="http://www.astrowars.com/$1" >\n$&};
 
 1;
