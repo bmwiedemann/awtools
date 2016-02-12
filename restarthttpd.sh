a9b3201a24df997970cd6098ee1786f14106610e
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2016-02-12 16:58:38 +0100

    make apache restart robust

--- restarthttpd.sh
+++ restarthttpd.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 LANG=C
-kill `cat /tmp/apache-brownie.pid` 
+mkdir -p /tmp/aw
+kill `cat /tmp/aw/apache-brownie.pid`
 n=10
 while test $n -gt 0 && killall -0 apache2 ; do sleep 1 ; n=$(expr $n - 1) ; done
-sleep 2; killall -9 apache2
+sleep 5; killall -9 apache2
+kill `pidof apache2`
 n=15
 while test $n -gt 0 && killall -0 apache2 ; do sleep 1 ; n=$(expr $n - 1) ; done
 #ln -s /host/log/brownie.log /tmp/
