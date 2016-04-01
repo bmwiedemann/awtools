c0d88d32d647d8c38de610ff2e1c28b74554b8fc
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2016-04-01 06:28:15 +0200

    mangle/trade: update to AW redesign

--- mangle/www1.astrowars.com/0/trade.pm
+++ mangle/www1.astrowars.com/0/trade.pm
@@ -1,7 +1,5 @@
 use strict;
 use DBAccess;
-s%<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" bgcolor='#000000' width="600"%$& class="main_inner" id="trade"%;
-
 
 # add artifact number info
 if(1 || $mangle::dispatch::g) {
@@ -19,8 +17,8 @@ if(1 || $mangle::dispatch::g) {
       my $p=$price/$baseprice;
       return int(0.5+(($p-1)*100)**(1.588971268));
    }
-   s%<td colspan="2"><b>Prices</b>%<td colspan="3"><b>Prices</b>%;
-   s%(<a href="?Stats/)([0-9a-z-]+)(\.html"?>[^<]+</a></td>)(<td align=right>\$)([0-9.,-]+)%"$1$2$3<td>".maparti($2,$5)."</td>$4$5"%ge;
+   s%<th scope="col">Price%<th scope="col">Num</th>$&%;
+   s%(<a href="?Stats/)([0-9a-z-]+)(\.html"?>[^<]+</a></td>)\s*(<td>\$)([0-9.,-]+)%"$1$2$3<td>".maparti($2,$5)."</td>$4$5"%gme;
    
    s%</body>%<span style="color:green">note: added number of total units sold in galaxy to artifacts table</span>$&%;
 }
